mpv-player/mpv

commit bb6b543812a72484fe657f04ae5c050d353b7584

command: provide per-file-options for loadfile command
Signed-off-by: wm4 <wm4@nowhere>

Closes #575. Minor changes over original pull request.

input/cmd_list.c
@@ -122,6 +122,7 @@ const struct mp_cmd_def mp_cmds[] = {
       ARG_STRING,
       OARG_CHOICE(0, ({"replace", 0},          {"0", 0},
                       {"append", 1},           {"1", 1})),
+      OPT_KEYVALUELIST(ARG(str_list), 0),
   }},
   { MP_CMD_LOADLIST, "loadlist", {
       ARG_STRING,

input/input.h
@@ -75,6 +75,7 @@ struct mp_cmd_arg {
         float f;
         double d;
         char *s;
+        char **str_list;
         void *p;
     } v;
 };

player/command.c
@@ -2973,7 +2973,15 @@ void run_command(MPContext *mpctx, mp_cmd_t *cmd)
         if (!append)
             playlist_clear(mpctx->playlist);
 
-        playlist_add(mpctx->playlist, playlist_entry_new(filename));
+        struct playlist_entry *entry = playlist_entry_new(filename);
+        if (cmd->args[2].v.str_list) {
+            char **pairs = cmd->args[2].v.str_list;
+            for (int i = 0; pairs[i] && pairs[i + 1]; i += 2) {
+                playlist_entry_add_param(entry, bstr0(pairs[i]),
+                                         bstr0(pairs[i + 1]));
+            }
+        }
+        playlist_add(mpctx->playlist, entry);
 
         if (!append)
             mp_set_playlist_entry(mpctx, mpctx->playlist->first);

dancingmirrors/dmpv

commit 719ba08b657095f9660e7ae545cf389f792b0962

command: fix loadfile command
This was broken by commit bb6b543812a724. Note that the original pull
request was fine, but it was broken by my own stupidity when I was
"improving" it.

The problem is that the new loadfile argument was not considered
optional anymore after my changes. The original pull request did handle
this by setting .defval to a dummy value, but I removed that part.

Fix it again by introducing a flag that designates that the parameter is
optional. (I didn't want to add it to m_option.h, because technically,
all options are optional, and it's not possible to have non-optional
options.)

input/cmd_list.c
@@ -122,7 +122,7 @@ const struct mp_cmd_def mp_cmds[] = {
       ARG_STRING,
       OARG_CHOICE(0, ({"replace", 0},          {"0", 0},
                       {"append", 1},           {"1", 1})),
-      OPT_KEYVALUELIST(ARG(str_list), 0),
+      OPT_KEYVALUELIST(ARG(str_list), MP_CMD_OPT_ARG),
   }},
   { MP_CMD_LOADLIST, "loadlist", {
       ARG_STRING,

input/cmd_list.h
@@ -24,6 +24,8 @@
 
 #define MP_CMD_MAX_ARGS 10
 
+#define MP_CMD_OPT_ARG 0x1000
+
 struct mp_cmd_def {
     int id;             // one of MP_CMD_...
     const char *name;   // user-visible name (as used in input.conf)

input/cmd_parse.c
@@ -199,10 +199,11 @@ static struct mp_cmd *parse_cmd(struct parse_ctx *ctx, int def_flags)
             if (is_vararg)
                 continue;
             // Skip optional arguments
-            if (opt->defval) {
+            if (opt->defval || (opt->flags & MP_CMD_OPT_ARG)) {
                 struct mp_cmd_arg *cmdarg = &cmd->args[cmd->nargs];
                 cmdarg->type = opt;
-                m_option_copy(opt, &cmdarg->v, opt->defval);
+                if (opt->defval)
+                    m_option_copy(opt, &cmdarg->v, opt->defval);
                 cmd->nargs++;
                 continue;
             }