dancingmirrors/dmpv

commit 965de719d15a7c4d508e8e54f1bcaed2bd4e408a

af_volume: separate softvol volume control from replaygain level
Currently, both replaygain adjustment and user volume control (if
softvol is enabled) share the same variable. Sharing the variable would
cause especially if --volume is used; then the replaygain volume would
always be overwritten.

Now both gain values are simple added right before doing filtering.

audio/filter/af_volume.c
@@ -31,7 +31,8 @@
 #include "demux/demux.h"
 
 struct priv {
-    float level;                // Gain level for each channel
+    float level;                // User-specified gain level for each channel
+    float rgain;                // Replaygain level
     int rgain_track;            // Enable/disable track based replaygain
     int rgain_album;            // Enable/disable album based replaygain
     float rgain_preamp;         // Set replaygain pre-amplification
@@ -129,10 +130,10 @@ static int control(struct af_instance *af, int cmd, void *arg)
                 !decode_peak(af, peak_tag, &peak))
             {
                 gain += s->rgain_preamp;
-                af_from_dB(1, &gain, &s->level, 20.0, -200.0, 60.0);
+                af_from_dB(1, &gain, &s->rgain, 20.0, -200.0, 60.0);
 
                 if (!s->rgain_clip) // clipping prevention
-                    s->level = MPMIN(s->level, 1.0 / peak);
+                    s->rgain = MPMIN(s->rgain, 1.0 / peak);
             }
         }
         return af_test_output(af, in);
@@ -151,9 +152,11 @@ static void filter_plane(struct af_instance *af, void *ptr, int num_samples)
 {
     struct priv *s = af->priv;
 
+    float level = s->level + s->rgain;
+
     if (af_fmt_from_planar(af->data->format) == AF_FORMAT_S16) {
         int16_t *a = ptr;
-        int vol = 256.0 * s->level;
+        int vol = 256.0 * level;
         if (vol != 256) {
             for (int i = 0; i < num_samples; i++) {
                 int x = (a[i] * vol) >> 8;
@@ -162,7 +165,7 @@ static void filter_plane(struct af_instance *af, void *ptr, int num_samples)
         }
     } else if (af_fmt_from_planar(af->data->format) == AF_FORMAT_FLOAT) {
         float *a = ptr;
-        float vol = s->level;
+        float vol = level;
         if (vol != 1.0) {
             for (int i = 0; i < num_samples; i++) {
                 float x = a[i] * vol;

commit 363971aafaf89ce19272951d9c8a765a155db2df

af_volume: fix replaygain
This was accidentally broken in commit 965de719. I somehow made the
wild assumption that replaygain adjusted the volume relative to 0%
instead of 100%.

The detach suboption was similarly broken.

audio/filter/af_volume.c
@@ -115,6 +115,7 @@ static int control(struct af_instance *af, int cmd, void *arg)
         }
         if (af_fmt_is_planar(in->format))
             mp_audio_set_format(af->data, af_fmt_to_planar(af->data->format));
+        s->rgain = 1.0;
         if ((s->rgain_track || s->rgain_album) && af->metadata) {
             float gain, peak;
             char *gain_tag = NULL, *peak_tag = NULL;
@@ -137,7 +138,7 @@ static int control(struct af_instance *af, int cmd, void *arg)
                     s->rgain = MPMIN(s->rgain, 1.0 / peak);
             }
         }
-        if (s->detach && fabs(s->level + s->rgain - 1.0) < 0.00001)
+        if (s->detach && fabs(s->level + s->rgain - 2.0) < 0.00001)
             return AF_DETACH;
         return af_test_output(af, in);
     }
@@ -155,7 +156,7 @@ static void filter_plane(struct af_instance *af, void *ptr, int num_samples)
 {
     struct priv *s = af->priv;
 
-    float level = s->level + s->rgain;
+    float level = s->level + s->rgain - 1.0;
 
     if (af_fmt_from_planar(af->data->format) == AF_FORMAT_S16) {
         int16_t *a = ptr;