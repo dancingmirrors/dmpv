dancingmirrors/dmpv

commit 97fd45187649b78510262f7a529e8f0a2ce601fc

cocoa: fix uninitialization while in fullscreen
This is only needed for switching video track with `_`, since Cocoa
automatically handles cleaning up the application's presentation options when
quitting the process.

Fixes #1399

video/out/cocoa/events_view.m
@@ -25,6 +25,7 @@
 #include "events_view.h"
 
 @interface MpvEventsView()
+@property(nonatomic, assign) BOOL clearing;
 @property(nonatomic, assign) BOOL hasMouseDown;
 @property(nonatomic, retain) NSTrackingArea *tracker;
 - (void)signalMousePosition;
@@ -36,6 +37,7 @@
 @end
 
 @implementation MpvEventsView
+@synthesize clearing = _clearing;
 @synthesize adapter = _adapter;
 @synthesize tracker = _tracker;
 @synthesize hasMouseDown = _mouse_down;
@@ -88,6 +90,12 @@
         [self.window makeKeyAndOrderFront:self];
         [self.window makeFirstResponder:self];
     }
+}
+
+- (void)clear
+{
+    self.clearing = YES;
+    [self exitFullScreenModeWithOptions:nil];
 }
 
 // mpv uses flipped coordinates, because X11 uses those. So let's just use them
@@ -177,6 +185,10 @@
 - (void)setFrameSize:(NSSize)size
 {
     [super setFrameSize:size];
+
+    if (self.clearing)
+        return;
+
     [self signalMousePosition];
 }
 

video/out/cocoa/events_view.h
@@ -21,5 +21,6 @@
 @interface MpvEventsView : NSView <NSDraggingDestination>
 @property(nonatomic, retain) MpvCocoaAdapter *adapter;
 - (void)setFullScreen:(BOOL)willBeFullscreen;
+- (void)clear;
 - (BOOL)canHideCursor;
 @end

video/out/cocoa_common.m
@@ -189,12 +189,24 @@ void vo_cocoa_register_resize_callback(struct vo *vo,
 
 void vo_cocoa_uninit(struct vo *vo)
 {
-    with_cocoa_lock(vo, ^{
-        struct vo_cocoa_state *s = vo->cocoa;
+    struct vo_cocoa_state *s = vo->cocoa;
+    NSView *ev = s->view;
+
+    // keep the event view around for later in order to call -clear
+    if (!s->embedded) {
+        [ev retain];
+    }
+
+    with_cocoa_lock_on_main_thread(vo, ^{
         enable_power_management(vo);
         cocoa_rm_fs_screen_profile_observer(vo);
 
         [s->gl_ctx release];
+
+        // needed to stop resize events triggered by the event's view -clear
+        // causing many uses after free
+        [s->video removeFromSuperview];
+
         [s->view removeFromSuperview];
         [s->view release];
 
@@ -202,6 +214,15 @@ void vo_cocoa_uninit(struct vo *vo)
         if (s->window)
             [s->window release];
     });
+
+    // don't use the mutex, because at that point it could have been destroyed
+    // and no one is accessing the events view anyway
+    if (!s->embedded) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [(MpvEventsView *)ev clear];
+            [ev release];
+        });
+    }
 }
 
 static int get_screen_handle(struct vo *vo, int identifier, NSWindow *window,

commit 59ed25a6830a988d59947bc269cf5b62a26688d2

cocoa: fix NSMapGet error
regression from 97fd451876

video/out/cocoa/events_view.m
@@ -94,8 +94,10 @@
 
 - (void)clear
 {
-    self.clearing = YES;
-    [self exitFullScreenModeWithOptions:nil];
+    if (self.inFullScreenMode) {
+        self.clearing = YES;
+        [self exitFullScreenModeWithOptions:nil];
+    }
 }
 
 // mpv uses flipped coordinates, because X11 uses those. So let's just use them