dancingmirrors/dmpv

commit 6f8b9716d35171d5eddb49759de4933e5b9d64e9

player: reset frame step counter on seeks
I suppose this wasn't done in order to keep the frame step counter
active even in the next file, but actually it was reset anyway.

player/playloop.c
@@ -154,6 +154,7 @@ void reset_playback_state(struct MPContext *mpctx)
     mpctx->playback_pts = MP_NOPTS_VALUE;
     mpctx->last_seek_pts = MP_NOPTS_VALUE;
     mpctx->cache_wait_time = 0;
+    mpctx->step_frames = 0;
     mpctx->restart_complete = false;
 
 #if HAVE_ENCODING

player/loadfile.c
@@ -903,7 +903,6 @@ static void play_current_file(struct MPContext *mpctx)
     mpctx->paused = false;
     mpctx->paused_for_cache = false;
     mpctx->playing_msg_shown = false;
-    mpctx->step_frames = 0;
     mpctx->backstep_active = false;
     mpctx->audio_delay = 0;
     mpctx->max_frames = -1;

commit 50f19b9c934bf05098429b896c9f59f11ac80977

player: fix framestep over timeline segment boundaries
This was subtly broken by commit 6f8b9716. Instead of framestepping over
the timeline segment boundary, it would just unpause playback, because
seeking now resets mpctx->step_frames. This was especially apparent when
doing something like "mpv *.jpg --merge-files".

Fix by restoring the step_frames field specifically if the seek is done
for switching segment boundaries. Hopefully the number fields which need
such an exception on seeking won't grow and turn this code into a mess.

player/playloop.c
@@ -169,6 +169,7 @@ static int mp_seek(MPContext *mpctx, struct seek_params seek,
 {
     struct MPOpts *opts = mpctx->opts;
     uint64_t prev_seek_ts = mpctx->vo_pts_history_seek_ts;
+    int prev_step = mpctx->step_frames;
 
     if (!mpctx->demuxer)
         return -1;
@@ -267,6 +268,7 @@ static int mp_seek(MPContext *mpctx, struct seek_params seek,
     if (timeline_fallthrough) {
         // Important if video reinit happens.
         mpctx->vo_pts_history_seek_ts = prev_seek_ts;
+        mpctx->step_frames = prev_step;
     } else {
         mpctx->vo_pts_history_seek_ts++;
         mpctx->backstep_active = false;