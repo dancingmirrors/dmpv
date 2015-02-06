mathstuf/mpv

tl_matroska: only check the chapter count when adding chapters
Fixes #1445 without failing in the case where file outside.mkv with N
chapters references inside.mkv with N+1 chapters. Since we don't care
about the number of chapters in inner files (yet; nested chapters would
change this, but also make chapters dynamic anyways), don't check there.

player/timeline/tl_matroska.c
@@ -396,10 +396,10 @@ static void build_timeline_loop(struct MPContext *mpctx,
             if (!demux_matroska_uid_cmp(&c->uid, &linked_m->uid))
                 continue;
 
-            if (i >= tl_info->num_chapters)
-                break; // probably needed only for broken sources
-
             if (!info->limit) {
+                if (i >= tl_info->num_chapters)
+                    break; // malformed files can cause this to happen.
+
                 chapters[i].pts = tl_info->start_time / 1e9;
                 chapters[i].name = talloc_strdup(chapters, c->name);
             }

dancingmirrors/dmpv

player/timeline/tl_matroska.c
@@ -396,10 +396,10 @@ static void build_timeline_loop(struct MPContext *mpctx,
             if (!demux_matroska_uid_cmp(&c->uid, &linked_m->uid))
                 continue;
 
-            if (i >= ctx->num_chapters)
-                break; // probably needed only for broken sources
-
             if (!info->limit) {
+                if (i >= ctx->num_chapters)
+                    break; // malformed files can cause this to happen.
+
                 chapters[i].pts = ctx->start_time / 1e9;
                 chapters[i].name = talloc_strdup(chapters, c->name);
             }