dancingmirrors/dmpv

commit 5ca6a63a1c5581a890ee5fc4281193850a04695e

audio: fix format function consistency issues
Replace all the check macros with function calls. Give them all the
same case and naming schema.

Drop af_fmt2bits(). Only af_fmt2bps() survives as af_fmt_to_bytes().

Introduce af_fmt_is_pcm(), and use it in situations that used
!AF_FORMAT_IS_SPECIAL. Nobody really knew what a "special" format
was. It simply meant "not PCM".

audio/out/ao_wasapi_utils.c
@@ -85,13 +85,13 @@ const struct wasapi_fmt_mapping wasapi_fmt_table[] = {
 
 static const GUID *format_to_subtype(int format)
 {
-    if (AF_FORMAT_IS_SPECIAL(format)) {
+    if (af_fmt_is_spdif(format)) {
         for (int i = 0; wasapi_fmt_table[i].format; i++) {
             if (wasapi_fmt_table[i].format == format)
                 return wasapi_fmt_table[i].subtype;
         }
         return &KSDATAFORMAT_SPECIFIER_NONE;
-    } else if (AF_FORMAT_IS_FLOAT(format)) {
+    } else if (af_fmt_is_float(format)) {
         return &KSDATAFORMAT_SUBTYPE_IEEE_FLOAT;
     }
     return &KSDATAFORMAT_SUBTYPE_PCM;
@@ -229,7 +229,7 @@ static void set_waveformat(WAVEFORMATEXTENSIBLE *wformat,
     wformat->Format.wFormatTag     = WAVE_FORMAT_EXTENSIBLE;
     wformat->Format.nChannels      = channels->num;
     wformat->Format.nSamplesPerSec = samplerate;
-    wformat->Format.wBitsPerSample = af_fmt2bits(format);
+    wformat->Format.wBitsPerSample = af_fmt_to_bytes(format) * 8;
     wformat->Format.cbSize = sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX);
 
     wformat->SubFormat                   = *format_to_subtype(format);
@@ -306,9 +306,11 @@ static int format_from_waveformat(WAVEFORMATEX *wf)
     // Since mpv doesn't have the notion of "valid bits", we just specify a
     // format with the container size. The least significant, "invalid"
     // bits will be excess precision ignored by wasapi.
-    // The change_bits operations should be a no-op for properly
+    // The change_bytes operations should be a no-op for properly
     // configured "special" formats, otherwise it will return 0.
-    return af_fmt_change_bits(format, wf->wBitsPerSample);
+    if (wf->wBitsPerSample % 8)
+        return 0;
+    return af_fmt_change_bytes(format, wf->wBitsPerSample / 8) * 8;
 }
 
 static bool chmap_from_waveformat(struct mp_chmap *channels, const WAVEFORMATEX *wf)
@@ -365,7 +367,7 @@ static bool set_ao_format(struct ao *ao, WAVEFORMATEX *wf, AUDCLNT_SHAREMODE sha
     }
 
     // Do not touch the ao for passthrough, just assume that we set WAVEFORMATEX correctly.
-    if (!AF_FORMAT_IS_SPECIAL(format)) {
+    if (af_fmt_is_pcm(format)) {
         struct mp_chmap channels;
         if (!chmap_from_waveformat(&channels, wf)) {
             MP_ERR(ao, "Unable to construct channel map from WAVEFORMAT %s\n",
@@ -575,7 +577,7 @@ static bool find_formats(struct ao *ao)
         // might be passthrough). If that fails, do a pcm format
         // search.
         return find_formats_exclusive(ao, true);
-    } else if (AF_FORMAT_IS_SPECIAL(ao->format)) {
+    } else if (af_fmt_is_spdif(ao->format)) {
         // If a passthrough format is requested, but exclusive mode
         // was not explicitly set, try only the requested passthrough
         // format in exclusive mode. Fall back on shared mode if that

audio/audio.c
@@ -34,8 +34,8 @@ static void update_redundant_info(struct mp_audio *mpa)
     assert(mp_chmap_is_empty(&mpa->channels) ||
            mp_chmap_is_valid(&mpa->channels));
     mpa->nch = mpa->channels.num;
-    mpa->bps = af_fmt2bps(mpa->format);
-    if (AF_FORMAT_IS_PLANAR(mpa->format)) {
+    mpa->bps = af_fmt_to_bytes(mpa->format);
+    if (af_fmt_is_planar(mpa->format)) {
         mpa->spf = 1;
         mpa->num_planes = mpa->nch;
         mpa->sstride = mpa->bps;
@@ -105,7 +105,7 @@ char *mp_audio_config_to_str_buf(char *buf, size_t buf_sz, struct mp_audio *mpa)
 
 void mp_audio_force_interleaved_format(struct mp_audio *mpa)
 {
-    if (AF_FORMAT_IS_PLANAR(mpa->format))
+    if (af_fmt_is_planar(mpa->format))
         mp_audio_set_format(mpa, af_fmt_from_planar(mpa->format));
 }
 

audio/audio.h
@@ -35,7 +35,7 @@ struct mp_audio {
     int nch;            // number of channels (redundant with chmap)
     int spf;            // sub-samples per sample on each plane
     int num_planes;     // number of planes
-    int bps;            // size of sub-samples (af_fmt2bps(format))
+    int bps;            // size of sub-samples (af_fmt_to_bytes(format))
 
     // --- private
     // These do not necessarily map directly to planes[]. They can have

audio/filter/af.c
@@ -493,8 +493,8 @@ static int af_reinit(struct af_stream *s)
             int fmt_in1 = af->prev->data->format;
             int fmt_in2 = in.format;
             if (af_fmt_is_valid(fmt_in1) && af_fmt_is_valid(fmt_in2)) {
-                bool spd1 = AF_FORMAT_IS_IEC61937(fmt_in1);
-                bool spd2 = AF_FORMAT_IS_IEC61937(fmt_in2);
+                bool spd1 = af_fmt_is_spdif(fmt_in1);
+                bool spd2 = af_fmt_is_spdif(fmt_in2);
                 if (spd1 != spd2 && af->next) {
                     MP_WARN(af, "Filter %s apparently cannot be used due to "
                                 "spdif passthrough - removing it.\n",

audio/filter/af_lavcac3enc.c
@@ -75,7 +75,7 @@ static int control(struct af_instance *af, int cmd, void *arg)
         struct mp_audio *in = arg;
         struct mp_audio orig_in = *in;
 
-        if (AF_FORMAT_IS_SPECIAL(in->format) || in->nch < s->cfg_min_channel_num)
+        if (!af_fmt_is_pcm(in->format) || in->nch < s->cfg_min_channel_num)
             return AF_DETACH;
 
         mp_audio_set_format(in, s->in_sampleformat);

audio/filter/af_lavrresample.c
@@ -497,7 +497,7 @@ static int filter(struct af_instance *af, struct mp_audio *in)
     mp_audio_copy_config(out, &s->avrctx_fmt);
 
     if (out->samples && !mp_audio_config_equals(out, &s->pre_out_fmt)) {
-        assert(AF_FORMAT_IS_PLANAR(out->format) && out->format == real_out.format);
+        assert(af_fmt_is_planar(out->format) && out->format == real_out.format);
         reorder_planes(out, s->reorder_out, &s->pool_fmt.channels);
         if (!mp_audio_config_equals(out, &s->pre_out_fmt)) {
             struct mp_audio *new = mp_audio_pool_get(s->reorder_buffer,

audio/filter/af_volume.c
@@ -68,7 +68,7 @@ static int control(struct af_instance *af, int cmd, void *arg)
         } else {
             mp_audio_set_format(af->data, AF_FORMAT_FLOAT);
         }
-        if (AF_FORMAT_IS_PLANAR(in->format))
+        if (af_fmt_is_planar(in->format))
             mp_audio_set_format(af->data, af_fmt_to_planar(af->data->format));
         s->rgain = 1.0;
         if ((s->rgain_track || s->rgain_album) && af->replaygain_data) {

audio/format.c
@@ -27,7 +27,8 @@
 #include "common/common.h"
 #include "audio/filter/af.h"
 
-int af_fmt2bps(int format)
+// number of bytes per sample, 0 if invalid/unknown
+int af_fmt_to_bytes(int format)
 {
     switch (af_fmt_from_planar(format)) {
     case AF_FORMAT_U8:      return 1;
@@ -42,27 +43,22 @@ int af_fmt2bps(int format)
     return 0;
 }
 
-int af_fmt2bits(int format)
+int af_fmt_change_bytes(int format, int bytes)
 {
-    return af_fmt2bps(format) * 8;
-}
-
-int af_fmt_change_bits(int format, int bits)
-{
-    if (!af_fmt_is_valid(format) || !bits)
+    if (!af_fmt_is_valid(format) || !bytes)
         return 0;
     for (int fmt = 1; fmt < AF_FORMAT_COUNT; fmt++) {
-        if (af_fmt2bits(fmt) == bits &&
+        if (af_fmt_to_bytes(fmt) == bytes &&
             af_fmt_is_float(fmt) == af_fmt_is_float(format) &&
             af_fmt_is_planar(fmt) == af_fmt_is_planar(format) &&
-            af_fmt_is_spdif(fmt) == af_fmt_is_planar(format))
+            af_fmt_is_spdif(fmt) == af_fmt_is_spdif(format))
             return fmt;
     }
     return 0;
 }
 
 // All formats are considered signed, except explicitly unsigned int formats.
-bool af_fmt_unsigned(int format)
+bool af_fmt_is_unsigned(int format)
 {
     return format == AF_FORMAT_U8 || format == AF_FORMAT_U8P;
 }
@@ -88,6 +84,11 @@ bool af_fmt_is_planar(int format)
 bool af_fmt_is_spdif(int format)
 {
     return af_format_sample_alignment(format) > 1;
+}
+
+bool af_fmt_is_pcm(int format)
+{
+    return af_fmt_is_valid(format) && !af_fmt_is_spdif(format);
 }
 
 static const int planar_formats[][2] = {
@@ -156,8 +157,8 @@ const char *af_fmt_to_str(int format)
 
 int af_fmt_seconds_to_bytes(int format, float seconds, int channels, int samplerate)
 {
-    assert(!AF_FORMAT_IS_PLANAR(format));
-    int bps      = af_fmt2bps(format);
+    assert(!af_fmt_is_planar(format));
+    int bps      = af_fmt_to_bytes(format);
     int framelen = channels * bps;
     int bytes    = seconds  * bps * samplerate;
     if (bytes % framelen)
@@ -167,7 +168,7 @@ int af_fmt_seconds_to_bytes(int format, float seconds, int channels, int sampler
 
 void af_fill_silence(void *dst, size_t bytes, int format)
 {
-    memset(dst, af_fmt_unsigned(format) ? 0x80 : 0, bytes);
+    memset(dst, af_fmt_is_unsigned(format) ? 0x80 : 0, bytes);
 }
 
 #define FMT_DIFF(type, a, b) (((a) & type) - ((b) & type))
@@ -183,28 +184,28 @@ int af_format_conversion_score(int dst_format, int src_format)
     if (dst_format == src_format)
         return 1024;
     // Can't be normally converted
-    if (AF_FORMAT_IS_SPECIAL(dst_format) || AF_FORMAT_IS_SPECIAL(src_format))
+    if (!af_fmt_is_pcm(dst_format) || !af_fmt_is_pcm(src_format))
         return INT_MIN;
     int score = 1024;
     if (af_fmt_is_planar(dst_format) != af_fmt_is_planar(src_format))
         score -= 1;     // has to (de-)planarize
     if (af_fmt_is_float(dst_format) != af_fmt_is_float(src_format)) {
-        int dst_bits = af_fmt2bits(dst_format);
+        int dst_bytes = af_fmt_to_bytes(dst_format);
         if (af_fmt_is_float(dst_format)) {
             // For int->float, always prefer 32 bit float.
-            score -= dst_bits == 32 ? 8 : 0;
+            score -= dst_bytes == 4 ? 1 : 0;
         } else {
             // For float->int, always prefer highest bit depth int
-            score -= 8 * (64 - dst_bits);
+            score -= 8 - dst_bytes;
         }
         // Has to convert float<->int - Consider this the worst case.
         score -= 2048;
     } else {
-        int bits = af_fmt2bits(dst_format) - af_fmt2bits(src_format);
-        if (bits > 0) {
-            score -= 8 * bits;          // has to add padding
-        } else if (bits < 0) {
-            score -= 1024 - 8 * bits;   // has to reduce bit depth
+        int bytes = af_fmt_to_bytes(dst_format) - af_fmt_to_bytes(src_format);
+        if (bytes > 0) {
+            score -= bytes;             // has to add padding
+        } else if (bytes < 0) {
+            score -= 1024 - bytes;      // has to reduce bit depth
         }
     }
     return score;

commit 3929a368ab15c99deddc5919ae06793470fd72cd

ao_wasapi: fix regression
This probably fixes the regression introduced with commit 5ca6a63a.

audio/out/ao_wasapi_utils.c
@@ -310,7 +310,7 @@ static int format_from_waveformat(WAVEFORMATEX *wf)
     // configured "special" formats, otherwise it will return 0.
     if (wf->wBitsPerSample % 8)
         return 0;
-    return af_fmt_change_bytes(format, wf->wBitsPerSample / 8) * 8;
+    return af_fmt_change_bytes(format, wf->wBitsPerSample / 8);
 }
 
 static bool chmap_from_waveformat(struct mp_chmap *channels, const WAVEFORMATEX *wf)