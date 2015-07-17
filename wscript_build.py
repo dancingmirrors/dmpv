dancingmirrors/dmpv

commit 55cbb5267f67a06c4f30e83130c637653622e988

build: make mpv.rc depend on version.h

wscript_build.py
@@ -418,6 +418,10 @@ def build(ctx):
                 ctx.path.find_node('osdep/mpv.rc'),
                 ctx.path.find_node(node))
 
+        ctx.add_manual_dependency(
+            ctx.path.find_node('osdep/mpv.rc'),
+            ctx.bldnode.find_node('version.h'))
+
     if ctx.dependency_satisfied('cplayer') or ctx.dependency_satisfied('test'):
         ctx(
             target       = "objects",

commit 2433dc7773ef86b8b6331b3cda5f113f6fc1985e

build: fix windows compilation after clean
broken since 55cbb5267f6

wscript_build.py
@@ -418,9 +418,11 @@ def build(ctx):
                 ctx.path.find_node('osdep/mpv.rc'),
                 ctx.path.find_node(node))
 
-        ctx.add_manual_dependency(
-            ctx.path.find_node('osdep/mpv.rc'),
-            ctx.bldnode.find_node('version.h'))
+        version = ctx.bldnode.find_node('version.h')
+        if version:
+            ctx.add_manual_dependency(
+                ctx.path.find_node('osdep/mpv.rc'),
+                version)
 
     if ctx.dependency_satisfied('cplayer') or ctx.dependency_satisfied('test'):
         ctx(