dancingmirrors/dmpv

commit 3c852b726cee55fc7e3eebd84cf3d46c40c12f33

audio: do not exit when loading small files in paused mode
When starting in paused mode, no audio is written to the device at all,
because writing audio implicitly unpauses the AO. If the file is very
small, and all audio fits within the AO buffer, this accidentally
triggered the EOF condition. (In unpaused mode, it would write all
audio, end playback, and then wait until the AO has everything played.)

player/audio.c
@@ -582,7 +582,7 @@ void fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
     mp_audio_buffer_skip(mpctx->ao_buffer, played);
 
     mpctx->audio_status = STATUS_PLAYING;
-    if (audio_eof) {
+    if (audio_eof && !mpctx->paused) {
         mpctx->audio_status = STATUS_DRAINING;
         // Wait until the AO has played all queued data. In the gapless case,
         // we trigger EOF immediately, and let it play asynchronously.

commit 1ae891d8da4c6c1707e6596aa04e1ee165430bb1

audio: fix EOF state with --keep-open
In paused mode, we never entered the audio EOF state. This shows e.g. in
--keep-open mode, which will not set the eof-reached property correctly.

Regression since commit 3c852b72. This commit was the wrong fix. We need
to respect the buffer state, and pausing has nothing to do with this.

Fixes #2167.

player/audio.c
@@ -609,7 +609,7 @@ void fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
     mp_audio_buffer_skip(mpctx->ao_buffer, played);
 
     mpctx->audio_status = STATUS_PLAYING;
-    if (audio_eof && !mpctx->paused) {
+    if (audio_eof && !mp_audio_buffer_samples(mpctx->ao_buffer)) {
         mpctx->audio_status = STATUS_DRAINING;
         // Wait until the AO has played all queued data. In the gapless case,
         // we trigger EOF immediately, and let it play asynchronously.