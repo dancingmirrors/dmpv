dancingmirrors/dmpv

commit c622155e7d93e0da320340dc6c1d460d6f5bb77f

vo: better magic value for unknown vsync interval
The value 1 is useful in some contexts, but not such a good choice
otherwise.

video/out/vo.c
@@ -985,7 +985,7 @@ int64_t vo_get_vsync_interval(struct vo *vo)
 {
     struct vo_internal *in = vo->in;
     pthread_mutex_lock(&in->lock);
-    int64_t res = vo->in->vsync_interval;
+    int64_t res = vo->in->vsync_interval > 1 ? vo->in->vsync_interval : -1;
     pthread_mutex_unlock(&in->lock);
     return res;
 }

video/out/vo_vdpau.c
@@ -723,6 +723,7 @@ static void flip_page(struct vo *vo)
     } else if (vc->user_fps == 0) {
         vc->vsync_interval = vo_get_vsync_interval(vo) * 1000;
     }
+    vc->vsync_interval = MPMAX(vc->vsync_interval, 1);
 
     if (duration > INT_MAX / 1000)
         duration = -1;

commit 258889e4f4ba95db790d020e556e0b3c84ab4fa2

vo_vdpau: fix frame scheduling if display FPS is unknown
Pretty stupid: vo_get_vsync_interval() returns a negative value if the
display FPS is unknown (e.g. xrandr not compiled), and the comparison
whether the value is below 0 fails later because it's assigned to an
unsigned int.

Regression since commit c622155e.

Also, fix some comments in vo.c.

video/out/vo.c
@@ -135,6 +135,8 @@ struct vo_internal {
     int queued_events;              // event mask for the user
     int internal_events;            // event mask for us
 
+    int64_t vsync_interval;
+
     int64_t flip_queue_offset; // queue flip events at most this much in advance
 
     int64_t drop_count;
@@ -151,7 +153,6 @@ struct vo_internal {
     double display_fps;
 
     // --- The following fields can be accessed from the VO thread only
-    int64_t vsync_interval;
     int64_t vsync_interval_approx;
     int64_t last_flip;
     char *window_title;
@@ -981,7 +982,6 @@ int vo_get_num_req_frames(struct vo *vo)
     return res;
 }
 
-// to be called from the VO thread only
 int64_t vo_get_vsync_interval(struct vo *vo)
 {
     struct vo_internal *in = vo->in;

video/out/vo_vdpau.c
@@ -110,7 +110,7 @@ struct vdpctx {
     VdpTime                            recent_vsync_time;
     float                              user_fps;
     int                                composite_detect;
-    unsigned int                       vsync_interval;
+    int                                vsync_interval;
     uint64_t                           last_queue_time;
     uint64_t                           queue_time[MAX_OUTPUT_SURFACES];
     uint64_t                           last_ideal_time;