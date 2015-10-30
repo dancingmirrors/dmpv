dancingmirrors/dmpv

commit 04d51c25c2c6857c638fcd72e5aeabaf45a5510e

vo: take normal drop path when dropping in display-sync mode
I hope there wasn't a deeper reason for exiting early.

video/out/vo.c
@@ -623,9 +623,6 @@ static bool render_frame(struct vo *vo)
         goto done;
     }
 
-    if (in->current_frame->display_synced && in->current_frame->num_vsyncs < 1)
-        goto done;
-
     frame = vo_frame_ref(in->current_frame);
     assert(frame);
 
@@ -701,8 +698,10 @@ static bool render_frame(struct vo *vo)
     // Setup parameters for the next time this frame is drawn. ("frame" is the
     // frame currently drawn, while in->current_frame is the potentially next.)
     in->current_frame->repeat = true;
-    if (frame->display_synced)
+    if (frame->display_synced) {
         in->current_frame->vsync_offset += in->vsync_interval;
+        in->dropped_frame |= in->current_frame->num_vsyncs < 1;
+    }
     if (in->current_frame->num_vsyncs > 0)
         in->current_frame->num_vsyncs -= 1;
 

commit b0826d8739a2225540ee8cdb3f52c0bc0ca81200

video: fix framedrop accounting in display-sync mode
Commit 04d51c25 broke this slightly. Frame drops got counted multiple
times, and also vo.c was actually trying to "render" the dropped frame
over and over again (normally not a problem, since frames are always
queued "tightly" in display-sync mode, but could have caused 100% CPU
usage in some rare corner cases).

Do not repeat already dropped frames, but still treat new frames with
num_vsyncs==0 as dropped frames. Also, strictly count dropped frames in
the VO. This means we don't count "soft" dropped frames anymore (frames
that are shown, but for fewer vsyncs than intended). This will be
adjusted in the next commit.

player/video.c
@@ -1022,8 +1022,6 @@ static void handle_display_sync_frame(struct MPContext *mpctx,
     // corner cases or exceptional situations cause too much havoc.
     drop_repeat = MPCLAMP(drop_repeat, -num_vsyncs, num_vsyncs * 10);
     num_vsyncs += drop_repeat;
-    if (drop_repeat < 0)
-        vo_increment_drop_count(vo, 1);
 
     // Estimate the video position, so we can calculate a good A/V difference
     // value below. This is used to estimate A/V drift.

video/out/vo.c
@@ -618,6 +618,7 @@ static bool render_frame(struct vo *vo)
         in->current_frame = in->frame_queued;
         in->frame_queued = NULL;
     } else if (in->paused || !in->current_frame || !in->hasframe ||
+               in->current_frame->num_vsyncs < 1 ||
                (!in->vsync_timed && !in->current_frame->display_synced))
     {
         goto done;