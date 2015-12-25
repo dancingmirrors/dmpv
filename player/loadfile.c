dancingmirrors/dmpv

commit dcd2bfb80490508dc9c20ce58cd0576ad609eec9

player: make timeline switching slightly nicer
But not much.

player/loadfile.c
@@ -286,10 +286,24 @@ static void enable_demux_thread(struct MPContext *mpctx)
     }
 }
 
-void timeline_set_part(struct MPContext *mpctx, int i, bool initial)
+// Returns whether reinitialization is required (i.e. it switched to a new part)
+bool timeline_switch_to_time(struct MPContext *mpctx, double pts)
 {
-    struct timeline_part *n = mpctx->timeline + i;
-    mpctx->timeline_part = i;
+    if (!mpctx->timeline)
+        return false;
+
+    int new_part = mpctx->num_timeline_parts - 1;
+    for (int i = 0; i < mpctx->num_timeline_parts; i++) {
+        if (pts < mpctx->timeline[i + 1].start) {
+            new_part = i;
+            break;
+        }
+    }
+
+    if (mpctx->timeline_part == new_part)
+        return false;
+    mpctx->timeline_part = new_part;
+    struct timeline_part *n = mpctx->timeline + mpctx->timeline_part;
 
     uninit_audio_chain(mpctx);
     uninit_video_chain(mpctx);
@@ -325,24 +339,12 @@ void timeline_set_part(struct MPContext *mpctx, int i, bool initial)
         }
     }
 
-    if (!initial) {
+    if (mpctx->playback_initialized) {
         reselect_demux_streams(mpctx);
         enable_demux_thread(mpctx);
     }
-}
 
-// Given pts, return the segment number of the corresponding part.
-int timeline_get_for_time(struct MPContext *mpctx, double pts)
-{
-    if (pts < 0)
-        pts = 0;
-
-    for (int i = 0; i < mpctx->num_timeline_parts; i++) {
-        if (pts < mpctx->timeline[i + 1].start)
-            return i;
-    }
-
-    return mpctx->num_timeline_parts - 1;
+    return true;
 }
 
 static int find_new_tid(struct MPContext *mpctx, enum stream_type t)
@@ -1118,9 +1120,8 @@ reopen_file:
     load_chapters(mpctx);
     add_demuxer_tracks(mpctx, mpctx->track_layout);
 
-    mpctx->timeline_part = 0;
-    if (mpctx->timeline)
-        timeline_set_part(mpctx, mpctx->timeline_part, true);
+    mpctx->timeline_part = mpctx->num_timeline_parts;
+    timeline_switch_to_time(mpctx, 0);
 
     open_subtitles_from_options(mpctx);
     open_audiofiles_from_options(mpctx);

player/core.h
@@ -430,7 +430,7 @@ void mp_switch_track_n(struct MPContext *mpctx, int order,
 void mp_deselect_track(struct MPContext *mpctx, struct track *track);
 struct track *mp_track_by_tid(struct MPContext *mpctx, enum stream_type type,
                               int tid);
-void timeline_set_part(struct MPContext *mpctx, int i, bool initial);
+bool timeline_switch_to_time(struct MPContext *mpctx, double pts);
 int timeline_get_for_time(struct MPContext *mpctx, double pts);
 void add_demuxer_tracks(struct MPContext *mpctx, struct demuxer *demuxer);
 bool mp_remove_track(struct MPContext *mpctx, struct track *track);

player/playloop.c
@@ -228,15 +228,11 @@ static int mp_seek(MPContext *mpctx, struct seek_params seek,
     hr_seek &= seek.type == MPSEEK_ABSOLUTE; // otherwise, no target PTS known
 
     double demuxer_amount = seek.amount;
-    if (mpctx->timeline) {
-        int segment = timeline_get_for_time(mpctx, seek.amount);
-        if (segment != mpctx->timeline_part) {
-            timeline_set_part(mpctx, segment, false);
-            reinit_video_chain(mpctx);
-            reinit_audio_chain(mpctx);
-            reinit_subs(mpctx, 0);
-            reinit_subs(mpctx, 1);
-        }
+    if (timeline_switch_to_time(mpctx, seek.amount)) {
+        reinit_video_chain(mpctx);
+        reinit_audio_chain(mpctx);
+        reinit_subs(mpctx, 0);
+        reinit_subs(mpctx, 1);
     }
 
     int demuxer_style = 0;

commit d3f33ef7f98be5f034ee1bf418880916055db26b

sub: clear subtitle list when crossing timeline boundary
When crossing timeline boundaries (such as switching to a new segment or
chapter with ordered chapters), clear the internal text subtitle list.
This breaks the sub-seek command, but is otherwise not too harmful.
Fixes Sub-OC-test-final7.mkv. (The internal text subtitle list is
basically a cache to make subtitles show up at the right time when
seeking back.)

I suspect this was caused by dcd2bfb8. The sample file times subtitles
slightly before the video frame when it should show up. This is to avoid
problems with subtitles showing up a frame later than intended. It also
means that a subtitle which is supposed to show up on the start of a
timeline part boundary actually might first be shown in a different
part. Since we now manipulate the packet timestamps, instead of
manipulating timestamps after the subtitle decoder, this means this
subtitle event would have 2 timestamps, which our code of course does
not handle.

If the two parts come one after another, this would actually work (since
the subtitle would have the same timestamps in the old and new part),
but it breaks if the new part (which follows the old part in the
physical file) is has a completely different start time in the timeline.

Essentially, the trick used to time subtitles correctly is incompatible
with the way we cache subtitles (to make them survive seeks).

The simple solution is just clearing the cached subtitles when crossing
chapter boundaries.

player/loadfile.c
@@ -336,6 +336,12 @@ bool timeline_switch_to_time(struct MPContext *mpctx, double pts)
                                                        track->type,
                                                        track->user_tid - 1);
             }
+
+            if (track->type == STREAM_SUB && track->stream) {
+                struct dec_sub *dec = track->stream->sub->dec_sub;
+                if (dec)
+                    sub_control(dec, SD_CTRL_CLEAR, NULL);
+            }
         }
     }
 

sub/dec_sub.h
@@ -22,6 +22,7 @@ enum sd_ctrl {
     SD_CTRL_SET_VIDEO_PARAMS,
     SD_CTRL_GET_RESOLUTION,
     SD_CTRL_SET_TOP,
+    SD_CTRL_CLEAR,
 };
 
 struct dec_sub *sub_create(struct mpv_global *global);

sub/sd_ass.c
@@ -510,13 +510,18 @@ static void fill_plaintext(struct sd *sd, double pts)
         track->styles[track->default_style].Alignment = ctx->on_top ? 6 : 2;
 }
 
+static void clear(struct sd *sd)
+{
+    struct sd_ass_priv *ctx = sd->priv;
+    ass_flush_events(ctx->ass_track);
+    ctx->num_seen_packets = 0;
+}
+
 static void reset(struct sd *sd)
 {
     struct sd_ass_priv *ctx = sd->priv;
-    if (sd->opts->sub_clear_on_seek) {
-        ass_flush_events(ctx->ass_track);
-        ctx->num_seen_packets = 0;
-    }
+    if (sd->opts->sub_clear_on_seek)
+        clear(sd);
     if (ctx->converter)
         lavc_conv_reset(ctx->converter);
 }
@@ -548,6 +553,9 @@ static int control(struct sd *sd, enum sd_ctrl cmd, void *arg)
         return CONTROL_OK;
     case SD_CTRL_SET_TOP:
         ctx->on_top = *(bool *)arg;
+        return CONTROL_OK;
+    case SD_CTRL_CLEAR:
+        clear(sd);
         return CONTROL_OK;
     default:
         return CONTROL_UNKNOWN;