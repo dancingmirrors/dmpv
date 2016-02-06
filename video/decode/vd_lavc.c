dancingmirrors/dmpv

commit a3f153134e222d5b52aacf78c2caebdedb523376

vd_lavc: avoid calling flush on an unopened AVCodecContext
It can be "dangerous". In particular, the decoder might have failed to
initialize, and is now in a broken state. avcodec_flush_buffers() is not
expected to be called in this state, and could trigger undefined
behavior.

video/decode/vd_lavc.c
@@ -457,6 +457,11 @@ static void init_avctx(struct dec_video *vd, const char *decoder,
 
 error:
     MP_ERR(vd, "Could not open codec.\n");
+    // Free it here to avoid attempting to flush+close.
+    if (ctx->avctx) {
+        av_freep(&ctx->avctx->extradata);
+        av_freep(&ctx->avctx);
+    }
     uninit_avctx(vd);
 }
 
@@ -483,23 +488,21 @@ static void flush_all(struct dec_video *vd)
 static void uninit_avctx(struct dec_video *vd)
 {
     vd_ffmpeg_ctx *ctx = vd->priv;
-    AVCodecContext *avctx = ctx->avctx;
 
     flush_all(vd);
     av_frame_free(&ctx->pic);
 
-    if (avctx) {
-        if (avctx->codec && avcodec_close(avctx) < 0)
+    if (ctx->avctx) {
+        if (avcodec_close(ctx->avctx) < 0)
             MP_ERR(vd, "Could not close codec.\n");
 
-        av_freep(&avctx->extradata);
+        av_freep(&ctx->avctx->extradata);
+        av_freep(&ctx->avctx);
     }
 
     if (ctx->hwdec && ctx->hwdec->uninit)
         ctx->hwdec->uninit(ctx);
     ctx->hwdec = NULL;
-
-    av_freep(&ctx->avctx);
 
     ctx->hwdec_failed = false;
     ctx->hwdec_fail_count = 0;

commit d84d464694b1b0187a37632be105b22cd2c5e93c

vd_lavc: fix use after free in some hwdecs
a3f153134e222d5b52aacf78c2caebdedb523376 introduced a regression that caused
segfault while uniniting dxva2 decoder (and possibly vdpau too). The problem was
that it freed the avctx earlier, before calling the backend-specific uninit
which referenced it.

Revert some of the changes of that commit, and avoid calling flush by
checking whether the codec is open instead.

(Based on a PR by Kevin Mitchell.)

Signed-off-by: wm4 <wm4@nowhere>

video/decode/vd_lavc.c
@@ -457,11 +457,6 @@ static void init_avctx(struct dec_video *vd, const char *decoder,
 
 error:
     MP_ERR(vd, "Could not open codec.\n");
-    // Free it here to avoid attempting to flush+close.
-    if (ctx->avctx) {
-        av_freep(&ctx->avctx->extradata);
-        av_freep(&ctx->avctx);
-    }
     uninit_avctx(vd);
 }
 
@@ -469,7 +464,7 @@ static void reset_avctx(struct dec_video *vd)
 {
     vd_ffmpeg_ctx *ctx = vd->priv;
 
-    if (ctx->avctx)
+    if (ctx->avctx && avcodec_is_open(ctx->avctx))
         avcodec_flush_buffers(ctx->avctx);
     ctx->flushing = false;
 }
@@ -495,14 +490,14 @@ static void uninit_avctx(struct dec_video *vd)
     if (ctx->avctx) {
         if (avcodec_close(ctx->avctx) < 0)
             MP_ERR(vd, "Could not close codec.\n");
-
         av_freep(&ctx->avctx->extradata);
-        av_freep(&ctx->avctx);
     }
 
     if (ctx->hwdec && ctx->hwdec->uninit)
         ctx->hwdec->uninit(ctx);
     ctx->hwdec = NULL;
+
+    av_freep(&ctx->avctx);
 
     ctx->hwdec_failed = false;
     ctx->hwdec_fail_count = 0;