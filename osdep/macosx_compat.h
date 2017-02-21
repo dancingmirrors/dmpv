dancingmirrors/dmpv

commit b16888b3b3f3ee5ccc065029579f2ae9fbc5b205

cocoa: only report mouse movements when window is not being dragged
even though the mouse doesn’t move relative to the window itself, when
the window is being dragged, some outliers are still reported and
trigger the OSC.

video/out/cocoa/events_view.m
@@ -211,6 +211,7 @@
 {
     if ([self.adapter mouseEnabled]) {
         [self mouseUpEvent:event];
+        [self.adapter mouseUp];
     } else {
         [super mouseUp:event];
     }

video/out/cocoa_common.m
@@ -76,6 +76,7 @@ struct vo_cocoa_state {
 
     bool cursor_visibility;
     bool cursor_visibility_wanted;
+    bool window_is_dragged;
 
     bool embedded; // wether we are embedding in another GUI
 
@@ -946,8 +947,9 @@ int vo_cocoa_control(struct vo *vo, int *events, int request, void *arg)
 
 - (void)signalMouseMovement:(NSPoint)point
 {
-    mp_input_set_mouse_pos(self.vout->input_ctx, point.x, point.y);
     [self recalcMovableByWindowBackground:point];
+    if (!self.vout->cocoa->window_is_dragged)
+        mp_input_set_mouse_pos(self.vout->input_ctx, point.x, point.y);
 }
 
 - (void)putKeyEvent:(NSEvent*)event
@@ -1050,6 +1052,16 @@ int vo_cocoa_control(struct vo *vo, int *events, int request, void *arg)
 - (void)windowDidDeminiaturize:(NSNotification *)notification
 {
     flag_events(self.vout, VO_EVENT_WIN_STATE);
+}
+
+- (void)windowWillMove:(NSNotification *)notification
+{
+    self.vout->cocoa->window_is_dragged = true;
+}
+
+- (void)mouseUp
+{
+    self.vout->cocoa->window_is_dragged = false;
 }
 
 @end

video/out/cocoa/window.m
@@ -204,6 +204,11 @@
     [self.adapter windowDidDeminiaturize:notification];
 }
 
+- (void)windowWillMove:(NSNotification *)notification
+{
+    [self.adapter windowWillMove:notification];
+}
+
 - (BOOL)canBecomeMainWindow { return YES; }
 - (BOOL)canBecomeKeyWindow { return YES; }
 - (BOOL)windowShouldClose:(id)sender

commit 83ab64b5b682e31d7993554a35a1e524fe0ea458

cocoa: fix dragging out of focus window
b16888b introduced a small regression where the cursor couldn't be
unhidden after refocusing. the problem is that no mouseUp event was
reported in our events_view. work around this with a separate event
monitor. this also fixes another regression when the window is being
dragged from the title bar.

#4174

osdep/macosx_compat.h
@@ -39,6 +39,7 @@ static const NSEventType NSEventTypeKeyUp = NSKeyUp;
 
 static const NSEventMask NSEventMaskKeyDown = NSKeyDownMask;
 static const NSEventMask NSEventMaskKeyUp = NSKeyUpMask;
+static const NSEventMask NSEventMaskLeftMouseUp = NSLeftMouseUpMask;
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_10)
 typedef NSUInteger NSEventModifierFlags;

video/out/cocoa/events_view.m
@@ -211,7 +211,6 @@
 {
     if ([self.adapter mouseEnabled]) {
         [self mouseUpEvent:event];
-        [self.adapter mouseUp];
     } else {
         [super mouseUp:event];
     }

video/out/cocoa_common.m
@@ -77,6 +77,7 @@ struct vo_cocoa_state {
     bool cursor_visibility;
     bool cursor_visibility_wanted;
     bool window_is_dragged;
+    id event_monitor_mouseup;
 
     bool embedded; // wether we are embedding in another GUI
 
@@ -352,6 +353,23 @@ static void vo_cocoa_uninit_displaylink(struct vo_cocoa_state *s)
     CVDisplayLinkRelease(s->link);
 }
 
+static void cocoa_add_event_monitor(struct vo *vo)
+{
+    struct vo_cocoa_state *s = vo->cocoa;
+
+    s->event_monitor_mouseup = [NSEvent
+        addLocalMonitorForEventsMatchingMask: NSEventMaskLeftMouseUp
+                                     handler:^NSEvent*(NSEvent* event) {
+            s->window_is_dragged = false;
+            return event;
+        }];
+}
+
+static void cocoa_rm_event_monitor(struct vo *vo)
+{
+    [NSEvent removeMonitor:vo->cocoa->event_monitor_mouseup];
+}
+
 void vo_cocoa_init(struct vo *vo)
 {
     struct vo_cocoa_state *s = talloc_zero(NULL, struct vo_cocoa_state);
@@ -372,6 +390,8 @@ void vo_cocoa_init(struct vo *vo)
     vo_cocoa_init_displaylink(vo);
     cocoa_init_light_sensor(vo);
     cocoa_add_screen_reconfiguration_observer(vo);
+    cocoa_add_event_monitor(vo);
+
     if (!s->embedded) {
         [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
         set_application_icon(NSApp);
@@ -426,6 +446,7 @@ void vo_cocoa_uninit(struct vo *vo)
         vo_cocoa_signal_swap(s);
         cocoa_uninit_light_sensor(s);
         cocoa_rm_screen_reconfiguration_observer(vo);
+        cocoa_rm_event_monitor(vo);
 
         [s->nsgl_ctx release];
         CGLReleaseContext(s->cgl_ctx);
@@ -1057,11 +1078,6 @@ int vo_cocoa_control(struct vo *vo, int *events, int request, void *arg)
 - (void)windowWillMove:(NSNotification *)notification
 {
     self.vout->cocoa->window_is_dragged = true;
-}
-
-- (void)mouseUp
-{
-    self.vout->cocoa->window_is_dragged = false;
 }
 
 @end