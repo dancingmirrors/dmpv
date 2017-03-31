dancingmirrors/dmpv

commit f4f5342af4c3bbc8479c991e74ffce43bf05a7e6

build: disable optical media libs by default (DVD/BD/CD)
Pure garbage.

wscript
@@ -329,19 +329,23 @@ iconv support use --disable-iconv.",
         'name': '--libbluray',
         'desc': 'Bluray support',
         'func': check_pkg_config('libbluray', '>= 0.3.0'),
+        #'default': 'disable',
     }, {
         'name': '--dvdread',
         'desc': 'dvdread support',
         'func': check_pkg_config('dvdread', '>= 4.1.0'),
+        'default': 'disable',
     }, {
         'name': '--dvdnav',
         'desc': 'dvdnav support',
         'deps': [ 'dvdread' ],
         'func': check_pkg_config('dvdnav', '>= 4.2.0'),
+        'default': 'disable',
     }, {
         'name': '--cdda',
         'desc': 'cdda support (libcdio)',
         'func': check_pkg_config('libcdio_paranoia'),
+        'default': 'disable',
     }, {
         'name': '--uchardet',
         'desc': 'uchardet support',

commit 1e5d61ab960d80732da473913cf33902293098de

wscript: decouple dvdnav check from dvdread
Reallows enabling dvdnav without enabling dvdread which was broken
in f4f5342af when they were both disabled by default.
Since dvdnav requires dvdread, we can enable dvdread:// even if
--enable-dvdread isn't passed.

Fixes #4290

stream/stream.c
@@ -93,7 +93,7 @@ static const stream_info_t *const stream_list[] = {
 #if HAVE_LIBSMBCLIENT
     &stream_info_smb,
 #endif
-#if HAVE_DVDREAD
+#if HAVE_DVDREAD || HAVE_DVDNAV
     &stream_info_ifo,
     &stream_info_dvd,
 #endif

wscript
@@ -338,8 +338,8 @@ iconv support use --disable-iconv.",
     }, {
         'name': '--dvdnav',
         'desc': 'dvdnav support',
-        'deps': [ 'dvdread' ],
-        'func': check_pkg_config('dvdnav', '>= 4.2.0'),
+        'func': check_pkg_config('dvdnav',  '>= 4.2.0',
+                                 'dvdread', '>= 4.1.0'),
         'default': 'disable',
     }, {
         'name': '--cdda',

wscript_build.py
@@ -260,7 +260,9 @@ def build(ctx):
         ( "stream/stream_cdda.c",                "cdda" ),
         ( "stream/stream_dvb.c",                 "dvbin" ),
         ( "stream/stream_dvd.c",                 "dvdread" ),
+        ( "stream/stream_dvd.c",                 "dvdnav" ),
         ( "stream/stream_dvd_common.c",          "dvdread" ),
+        ( "stream/stream_dvd_common.c",          "dvdnav" ),
         ( "stream/stream_dvdnav.c",              "dvdnav" ),
         ( "stream/stream_edl.c" ),
         ( "stream/stream_file.c" ),