dancingmirrors/dmpv

commit d41ec7c17097978792418425f5ab9a19fe30230a

scripting: expand --script filename for C plugins

player/scripting.c
@@ -215,8 +215,11 @@ void mp_load_scripts(struct MPContext *mpctx)
     // Load scripts from options
     char **files = mpctx->opts->script_files;
     for (int n = 0; files && files[n]; n++) {
-        if (files[n][0])
-            mp_load_script(mpctx, files[n]);
+        if (files[n][0]) {
+            char *path = mp_get_user_path(NULL, mpctx->global, files[n]);
+            mp_load_script(mpctx, path);
+            talloc_free(path);
+        }
     }
     if (!mpctx->opts->auto_load_scripts)
         return;

player/lua.c
@@ -190,10 +190,8 @@ static void add_functions(struct script_ctx *ctx);
 static void load_file(lua_State *L, const char *fname)
 {
     struct script_ctx *ctx = get_ctx(L);
-    char *res_name = mp_get_user_path(NULL, ctx->mpctx->global, fname);
-    MP_VERBOSE(ctx, "loading file %s\n", res_name);
-    int r = luaL_loadfile(L, res_name);
-    talloc_free(res_name); // careful to not leak this on Lua errors
+    MP_VERBOSE(ctx, "loading file %s\n", fname);
+    int r = luaL_loadfile(L, fname);
     if (r)
         lua_error(L);
     lua_call(L, 0, 0);

commit b32dce05a8db89363efa4dee8e46e937e07861db

scripting: add wrapper to load scripts with user paths
Fixes regression since d41ec7c170, which broke load-script command
when used with user paths (ex: ~~/script.lua)

player/command.c
@@ -5643,7 +5643,7 @@ int run_command(struct MPContext *mpctx, struct mp_cmd *cmd, struct mpv_node *re
 
     case MP_CMD_LOAD_SCRIPT: {
         char *script = cmd->args[0].v.s;
-        if (mp_load_script(mpctx, script) < 0)
+        if (mp_load_user_script(mpctx, script) < 0)
             return -1;
         break;
     }

player/core.h
@@ -594,6 +594,7 @@ struct mp_scripting {
 void mp_load_scripts(struct MPContext *mpctx);
 void mp_load_builtin_scripts(struct MPContext *mpctx);
 int mp_load_script(struct MPContext *mpctx, const char *fname);
+int mp_load_user_script(struct MPContext *mpctx, const char *fname);
 
 // sub.c
 void reset_subtitle_state(struct MPContext *mpctx);

player/scripting.c
@@ -155,6 +155,14 @@ int mp_load_script(struct MPContext *mpctx, const char *fname)
     return 0;
 }
 
+int mp_load_user_script(struct MPContext *mpctx, const char *fname)
+{
+    char *path = mp_get_user_path(NULL, mpctx->global, fname);
+    int ret = mp_load_script(mpctx, path);
+    talloc_free(path);
+    return ret;
+}
+
 static int compare_filename(const void *pa, const void *pb)
 {
     char *a = (char *)pa;
@@ -219,11 +227,8 @@ void mp_load_scripts(struct MPContext *mpctx)
     // Load scripts from options
     char **files = mpctx->opts->script_files;
     for (int n = 0; files && files[n]; n++) {
-        if (files[n][0]) {
-            char *path = mp_get_user_path(NULL, mpctx->global, files[n]);
-            mp_load_script(mpctx, path);
-            talloc_free(path);
-        }
+        if (files[n][0])
+            mp_load_user_script(mpctx, files[n]);
     }
     if (!mpctx->opts->auto_load_scripts)
         return;