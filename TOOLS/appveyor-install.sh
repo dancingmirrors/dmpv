dancingmirrors/dmpv

commit 52c5673abe199cda5c43b3a494807fb5db079d1c

Remove support for ffmpeg-mpv

TOOLS/appveyor-install.sh
@@ -27,9 +27,9 @@ pacman -S --noconfirm --needed \
 # Delete unused packages to reduce space used in the Appveyor cache
 pacman -Sc --noconfirm
 
-# Compile ffmpeg-mpv
+# Compile ffmpeg
 (
-    git clone --depth=1 https://github.com/mpv-player/ffmpeg-mpv.git && cd ffmpeg-mpv
+    git clone --depth=1 https://git.ffmpeg.org/ffmpeg.git && cd ffmpeg
 
     mkdir build && cd build
     ../configure \

TOOLS/travis-deps
@@ -93,7 +93,7 @@ class Libav < TravisDepsBuilder
       },
       "ffmpeg-git" => {
         :action => :git,
-        :url    => "git://github.com/mpv-player/ffmpeg-mpv.git"
+        :url    => "git://git.ffmpeg.org/ffmpeg.git"
       }
     }
   end

wscript
@@ -444,15 +444,8 @@ libav_dependencies = [
         'req': True,
         'fmsg': "FFmpeg/Libav development files not found.",
     }, {
-        'name': 'ffmpeg-mpv',
-        'desc': 'libav* is FFmpeg mpv modified version',
-        'func': check_statement('libavcodec/version.h',
-                                'int x[LIBAVCODEC_MPV ? 1 : -1]',
-                                use='libavcodec')
-    }, {
-        'name': '--ffmpeg-upstream',
-        'deps': '!ffmpeg-mpv',
-        'desc': 'libav* is upstream FFmpeg (unsupported)',
+        'name': 'ffmpeg',
+        'desc': 'libav* is FFmpeg',
         # FFmpeg <=> LIBAVUTIL_VERSION_MICRO>=100
         'func': check_statement('libavcodec/version.h',
                                 'int x[LIBAVCODEC_VERSION_MICRO >= 100 ? 1 : -1]',
@@ -470,13 +463,11 @@ libav_dependencies = [
     }, {
         'name': 'libav-any',
         'desc': 'Libav/FFmpeg library versions',
-        'deps': 'ffmpeg-mpv || ffmpeg-upstream || libav',
+        'deps': 'ffmpeg || libav',
         'func': check_ffmpeg_or_libav_versions(),
         'req': True,
         'fmsg': "Unable to find development files for some of the required \
-FFmpeg/Libav libraries. You need git master. For FFmpeg, the mpv fork, that \
-might contain additional fixes and features is required. It is available on \
-https://github.com/mpv-player/ffmpeg-mpv Aborting."
+FFmpeg/Libav libraries. Git master is recommended."
     }, {
         'name': '--libavdevice',
         'desc': 'libavdevice',

commit 6d48a0b57bf21964ebe7d638881d315603e39dbf

appveyor: fix FFmpeg download
Broken in 52c5673abe19. It seems like the Git server on git.ffmpeg.org
doesn't like clone --depth=1, so use the GitHub mirror instead.

TOOLS/appveyor-install.sh
@@ -29,7 +29,7 @@ pacman -Sc --noconfirm
 
 # Compile ffmpeg
 (
-    git clone --depth=1 https://git.ffmpeg.org/ffmpeg.git && cd ffmpeg
+    git clone --depth=1 https://github.com/FFmpeg/ffmpeg.git && cd ffmpeg
 
     mkdir build && cd build
     ../configure \