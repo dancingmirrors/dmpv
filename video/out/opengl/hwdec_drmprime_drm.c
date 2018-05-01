LongChair/mpv

drm/atomic: add connector to atomic context
This patch adds
  - DRM connector object to atomic context.
  - fd property to the drm atomic object as well as a method to read blob type properties.
  - connector id to the opengl-cb-drm-params interop struct.

This allows to ensure that the proper connector is picked up, especially when specifying it
from the commandline, and also allows to make sure we're using the right one when embedding
with interop into an application.

video/out/opengl/hwdec_drmprime_drm.c
@@ -229,7 +229,7 @@ static int init(struct ra_hwdec *hw)
 
     if (params->fd) {
         p->ctx = drm_atomic_create_context(p->log, params->fd, params->crtc_id,
-                                           drm_overlay);
+                                           params->connector_id, drm_overlay);
         if (!p->ctx) {
             mp_err(p->log, "Failed to retrieve DRM atomic context.\n");
             goto err;

dancingmirrors/dmpv

video/out/opengl/hwdec_drmprime_drm.c
@@ -224,7 +224,7 @@ static int init(struct ra_hwdec *hw)
     drm_params = ra_get_native_resource(hw->ra, "drm_params");
     if (drm_params) {
         p->ctx = drm_atomic_create_context(p->log, drm_params->fd, drm_params->crtc_id,
-                                           drm_overlay);
+                                           drm_params->connector_id, drm_overlay);
         if (!p->ctx) {
             mp_err(p->log, "Failed to retrieve DRM atomic context.\n");
             goto err;