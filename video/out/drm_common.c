xantoz/mpv

drm: refactor page_flipped callback
Avoid duplicating the same callback function in both context_drm_egl
and vo_drm.

video/out/drm_common.c
@@ -25,13 +25,16 @@
 #include <unistd.h>
 #include <limits.h>
 #include <math.h>
+#include <time.h>
 
 #include "drm_common.h"
 
 #include "common/common.h"
 #include "common/msg.h"
 #include "osdep/io.h"
+#include "osdep/timer.h"
 #include "misc/ctype.h"
+#include "video/out/vo.h"
 
 #define EVT_RELEASE 1
 #define EVT_ACQUIRE 2
@@ -903,4 +906,49 @@ void vt_switcher_poll(struct vt_switcher *s, int timeout_ms)
     case EVT_INTERRUPT:
         break;
     }
+}
+
+void page_flipped(int fd, unsigned int msc, unsigned int sec,
+                  unsigned int usec, void *data)
+{
+    struct drm_pflip_cb_closure *closure = data;
+
+    struct drm_vsync_tuple *vsync = closure->vsync;
+    // frame_vsync->ust is the timestamp of the pageflip that happened just before this flip was queued
+    // frame_vsync->msc is the sequence number of the pageflip that happened just before this flip was queued
+    // frame_vsync->sbc is the sequence number for the frame that was just flipped to screen
+    struct drm_vsync_tuple *frame_vsync = closure->frame_vsync;
+    struct vo_vsync_info *vsync_info = closure->vsync_info;
+
+    const bool ready =
+        (vsync->msc != 0) &&
+        (frame_vsync->ust != 0) && (frame_vsync->msc != 0);
+
+    const uint64_t ust = (sec * 1000000LL) + usec;
+
+    const unsigned int msc_since_last_flip = msc - vsync->msc;
+
+    vsync->ust = ust;
+    vsync->msc = msc;
+
+    if (ready) {
+        // Convert to mp_time
+        struct timespec ts;
+        if (clock_gettime(CLOCK_MONOTONIC, &ts))
+            goto fail;
+        const uint64_t now_monotonic = ts.tv_sec * 1000000LL + ts.tv_nsec / 1000;
+        const uint64_t ust_mp_time = mp_time_us() - (now_monotonic - vsync->ust);
+
+        const uint64_t     ust_since_enqueue = vsync->ust - frame_vsync->ust;
+        const unsigned int msc_since_enqueue = vsync->msc - frame_vsync->msc;
+        const unsigned int sbc_since_enqueue = vsync->sbc - frame_vsync->sbc;
+
+        vsync_info->vsync_duration = ust_since_enqueue / msc_since_enqueue;
+        vsync_info->skipped_vsyncs = msc_since_last_flip - 1; // Valid iff swap_buffers is called every vsync
+        vsync_info->last_queue_display_time = ust_mp_time + (sbc_since_enqueue * vsync_info->vsync_duration);
+    }
+
+fail:
+    *closure->waiting_for_flip = false;
+    talloc_free(closure);
 }

video/out/drm_common.h
@@ -61,6 +61,13 @@ struct drm_vsync_tuple {
     unsigned int sbc;
 };
 
+struct drm_pflip_cb_closure {
+    struct drm_vsync_tuple *frame_vsync; // vsync tuple when the frame that just flipped was queued
+    struct drm_vsync_tuple *vsync; // vsync tuple of the latest page flip. page_flipped updates this
+    struct vo_vsync_info *vsync_info; // where the page_flipped routine writes its output
+    bool *waiting_for_flip; // page_flipped writes false here before returning
+};
+
 bool vt_switcher_init(struct vt_switcher *s, struct mp_log *log);
 void vt_switcher_destroy(struct vt_switcher *s);
 void vt_switcher_poll(struct vt_switcher *s, int timeout_ms);
@@ -77,5 +84,8 @@ struct kms *kms_create(struct mp_log *log, const char *connector_spec,
                        bool use_atomic);
 void kms_destroy(struct kms *kms);
 double kms_get_display_fps(const struct kms *kms);
+
+void page_flipped(int fd, unsigned int msc, unsigned int sec,
+                  unsigned int usec, void *data);
 
 #endif

dancingmirrors/dmpv

video/out/drm_common.c
@@ -25,13 +25,16 @@
 #include <unistd.h>
 #include <limits.h>
 #include <math.h>
+#include <time.h>
 
 #include "drm_common.h"
 
 #include "common/common.h"
 #include "common/msg.h"
 #include "osdep/io.h"
+#include "osdep/timer.h"
 #include "misc/ctype.h"
+#include "video/out/vo.h"
 
 #define EVT_RELEASE 1
 #define EVT_ACQUIRE 2
@@ -899,4 +902,49 @@ void vt_switcher_poll(struct vt_switcher *s, int timeout_ms)
     case EVT_INTERRUPT:
         break;
     }
+}
+
+void drm_pflip_cb(int fd, unsigned int msc, unsigned int sec,
+                  unsigned int usec, void *data)
+{
+    struct drm_pflip_cb_closure *closure = data;
+
+    struct drm_vsync_tuple *vsync = closure->vsync;
+    // frame_vsync->ust is the timestamp of the pageflip that happened just before this flip was queued
+    // frame_vsync->msc is the sequence number of the pageflip that happened just before this flip was queued
+    // frame_vsync->sbc is the sequence number for the frame that was just flipped to screen
+    struct drm_vsync_tuple *frame_vsync = closure->frame_vsync;
+    struct vo_vsync_info *vsync_info = closure->vsync_info;
+
+    const bool ready =
+        (vsync->msc != 0) &&
+        (frame_vsync->ust != 0) && (frame_vsync->msc != 0);
+
+    const uint64_t ust = (sec * 1000000LL) + usec;
+
+    const unsigned int msc_since_last_flip = msc - vsync->msc;
+
+    vsync->ust = ust;
+    vsync->msc = msc;
+
+    if (ready) {
+        // Convert to mp_time
+        struct timespec ts;
+        if (clock_gettime(CLOCK_MONOTONIC, &ts))
+            goto fail;
+        const uint64_t now_monotonic = ts.tv_sec * 1000000LL + ts.tv_nsec / 1000;
+        const uint64_t ust_mp_time = mp_time_us() - (now_monotonic - vsync->ust);
+
+        const uint64_t     ust_since_enqueue = vsync->ust - frame_vsync->ust;
+        const unsigned int msc_since_enqueue = vsync->msc - frame_vsync->msc;
+        const unsigned int sbc_since_enqueue = vsync->sbc - frame_vsync->sbc;
+
+        vsync_info->vsync_duration = ust_since_enqueue / msc_since_enqueue;
+        vsync_info->skipped_vsyncs = msc_since_last_flip - 1; // Valid iff swap_buffers is called every vsync
+        vsync_info->last_queue_display_time = ust_mp_time + (sbc_since_enqueue * vsync_info->vsync_duration);
+    }
+
+fail:
+    *closure->waiting_for_flip = false;
+    talloc_free(closure);
 }

video/out/drm_common.h
@@ -61,6 +61,13 @@ struct drm_vsync_tuple {
     unsigned int sbc;
 };
 
+struct drm_pflip_cb_closure {
+    struct drm_vsync_tuple *frame_vsync; // vsync tuple when the frame that just flipped was queued
+    struct drm_vsync_tuple *vsync; // vsync tuple of the latest page flip. drm_pflip_cb updates this
+    struct vo_vsync_info *vsync_info; // where the drm_pflip_cb routine writes its output
+    bool *waiting_for_flip; // drm_pflip_cb writes false here before returning
+};
+
 bool vt_switcher_init(struct vt_switcher *s, struct mp_log *log);
 void vt_switcher_destroy(struct vt_switcher *s);
 void vt_switcher_poll(struct vt_switcher *s, int timeout_ms);
@@ -77,5 +84,9 @@ struct kms *kms_create(struct mp_log *log, const char *connector_spec,
                        bool use_atomic);
 void kms_destroy(struct kms *kms);
 double kms_get_display_fps(const struct kms *kms);
+
+// DRM Page Flip callback
+void drm_pflip_cb(int fd, unsigned int msc, unsigned int sec,
+                  unsigned int usec, void *data);
 
 #endif