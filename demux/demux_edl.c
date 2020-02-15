dancingmirrors/dmpv

commit b5f2b909338c38584418f36488c59c63e7b213d7

demux_edl: restore relative path resolution
Playing e.g. "dir/f.edl" should make all non-absolute paths in f.edl
relative to "dir".

Commit 0948e516bef accidentally broke this.

demux/demux_edl.c
@@ -397,6 +397,17 @@ error:
     return NULL;
 }
 
+static void fix_filenames(struct tl_parts *parts, char *source_path)
+{
+    if (bstr_equals0(mp_split_proto(bstr0(source_path), NULL), "edl"))
+        return;
+    struct bstr dirname = mp_dirname(source_path);
+    for (int n = 0; n < parts->num_parts; n++) {
+        struct tl_part *part = &parts->parts[n];
+        part->filename = mp_path_join_bstr(parts, dirname, bstr0(part->filename));
+    }
+}
+
 static void build_mpv_edl_timeline(struct timeline *tl)
 {
     struct priv *p = tl->demuxer->priv;
@@ -413,6 +424,7 @@ static void build_mpv_edl_timeline(struct timeline *tl)
 
     for (int n = 0; n < root->num_pars; n++) {
         struct tl_parts *parts = root->pars[n];
+        fix_filenames(parts, tl->demuxer->filename);
         struct timeline_par *par = build_timeline(tl, parts);
         if (!par)
             break;

commit fac96c707ecc3069fc5d5018dee6a3faaa642eda

demux_edl: accept protocol entries in EDL entries again
Accidentally broken by commit b5f2b909338c38. mp_path_join() does not
check for this, because it's supposed to work on filesystem strings (and
e.g. "http://fubar" is a valid relative path in UNIX).

demux/demux_edl.c
@@ -458,7 +458,10 @@ static void fix_filenames(struct tl_parts *parts, char *source_path)
     struct bstr dirname = mp_dirname(source_path);
     for (int n = 0; n < parts->num_parts; n++) {
         struct tl_part *part = &parts->parts[n];
-        part->filename = mp_path_join_bstr(parts, dirname, bstr0(part->filename));
+        if (!mp_is_url(bstr0(part->filename))) {
+            part->filename =
+                mp_path_join_bstr(parts, dirname, bstr0(part->filename));
+        }
     }
 }
 