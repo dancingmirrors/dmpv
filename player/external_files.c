dancingmirrors/dmpv

commit a4aa15e95cdfbfd47230ade1902bc3c576a8d3d4

player: reorder list of external cover files for optimal results

player/external_files.c
@@ -44,20 +44,22 @@ static const char *const audio_exts[] = {"mp3", "aac", "mka", "dts", "flac",
 
 // Stolen from: vlc/-/blob/master/modules/meta_engine/folder.c#L40
 static const char *const cover_files[] = {
-    "Folder.jpg",
-    "Folder.png",
-    "AlbumArtSmall.jpg",
     "AlbumArt.jpg",
     "Album.jpg",
-    ".folder.png",
     "cover.jpg",
     "cover.png",
-    "cover.gif",
     "front.jpg",
     "front.png",
-    "front.gif",
-    "front.bmp",
+
+    "AlbumArtSmall.jpg",
+    "Folder.jpg",
+    "Folder.png",
+    ".folder.png",
     "thumb.jpg",
+
+    "front.bmp",
+    "front.gif",
+    "cover.gif",
     NULL
 };
 

commit 56ac2af8669b696c76688524680d34f6c0710f5d

player: fix external cover file prioritization
Array order was ignored entirely instead of being used as intended.
Fixes: a4aa15e95c

player/external_files.c
@@ -43,6 +43,7 @@ static const char *const audio_exts[] = {"mp3", "aac", "mka", "dts", "flac",
                                          NULL};
 
 // Stolen from: vlc/-/blob/master/modules/meta_engine/folder.c#L40
+// sorted by priority (descending)
 static const char *const cover_files[] = {
     "AlbumArt.jpg",
     "Album.jpg",
@@ -81,11 +82,13 @@ static int test_ext(bstr ext)
     return -1;
 }
 
-static int test_filename(bstr fname)
+static int test_cover_filename(bstr fname, int *priority)
 {
     for (int n = 0; cover_files[n]; n++) {
-        if (bstrcasecmp(bstr0(cover_files[n]), fname) == 0)
+        if (bstrcasecmp(bstr0(cover_files[n]), fname) == 0) {
+            *priority = MP_ARRAY_SIZE(cover_files) - n;
             return STREAM_VIDEO;
+        }
     }
     return -1;
 }
@@ -188,9 +191,10 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
             talloc_steal(tmpmem2, dename.start);
 
         // check what it is (most likely)
+        int cover_prio = 0;
         int type = test_ext(tmp_fname_ext);
         if (type < 0)
-            type = test_filename(dename);
+            type = test_cover_filename(dename, &cover_prio);
         char **langs = NULL;
         int fuzz = -1;
         switch (type) {
@@ -247,7 +251,7 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
 
         // cover art: just accept it
         if (type == STREAM_VIDEO && fuzz >= 1)
-            prio |= 1;
+            prio = cover_prio;
 
         mp_dbg(log, "Potential external file: \"%s\"  Priority: %d\n",
                de->d_name, prio);