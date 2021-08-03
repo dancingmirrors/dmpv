dancingmirrors/dmpv

commit db53f2dfc197369a38b4db7e93ba6c7d5c84c21d

wayland: unset hidden state in frame callback
More wayland weirdness. So previously, flipping a hidden state from true
to false was done in vo_wayland_wait_frame. In theory, this would be
after you get the frame callback and all those events so there's no
problem. However since the function also does a bunch of
flushing/dispatching/etc. to the default display queue so a lot of
unknown things can happen before we actually set the hidden variable
back to false. For example if a single image was paused and left on
another virtual desktop long enough (~5 minutes) while also not having
focus, switching back to that desktop could render it a black frame.

This edge case was supposed to be handled by the surface being activated
again in the toplevel event but apparently that doesn't always work. The
fix is to just delete all of that junk and set wl->hidden = false in the
frame callback. What's actually happening is kind of a mystery honestly.
Probably the compositor drops the buffers after a while as an
optimization (sensible) and forces a repaint if you switch back to the
virtual desktop. Somehow wl->hidden not being set to false would not
properly trigger a repaint (likely because it also sends a toplevel
event which does stuff) thus you just get a black window. If you just
make sure to set hidden in the frame callback, it appears like all of
these problems and edge cases are solved. Since this event must happen
first, that makes sense. That simplifies a lot of stuff and fixes some
subtle bugs at the same time so just go with this approach.

video/out/wayland_common.c
@@ -838,12 +838,6 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
         {
             wl->focused = !wl->focused;
             wl->pending_vo_events |= VO_EVENT_FOCUS;
-
-            if (wl->activated) {
-                /* If the surface comes back into view, force a redraw. */
-                vo_wayland_wait_frame(wl);
-                wl->pending_vo_events |= VO_EVENT_EXPOSE;
-            }
         }
     }
 
@@ -994,6 +988,7 @@ static void frame_callback(void *data, struct wl_callback *callback, uint32_t ti
     }
 
     wl->frame_wait = false;
+    wl->hidden = false;
 }
 
 static const struct wl_callback_listener frame_listener = {
@@ -1992,13 +1987,11 @@ void vo_wayland_wait_frame(struct vo_wayland_state *wl)
             return;
         } else {
             wl->timeout_count += 1;
-            wl->hidden = false;
             return;
         }
     }
 
     wl->timeout_count = 0;
-    wl->hidden = false;
 }
 
 void vo_wayland_wait_events(struct vo *vo, int64_t until_time_us)

commit 7584fd73dcd1910103a49b39146c744f3ab90fe3

wayland: cleanup handle_toplevel_config
The source of many geometry woes. There's some loosely related toplevel
things that should be cleaned up/fixed. First of all,
VO_EVENT_LIVE_RESIZING is actually completely useless. It might have
been useful sometime in the past, but there's no point. It doesn't
"speed up" resizing in any way and appears to be originally for cocoa.
Just remove it.

Way back in the day, toplevel_width/height was added as a workaround for
when we got uncoorperative (i.e. wrong) width/height coordinates from
the compositor in this event. Basically it could happen due to numerous
reasons but a lack of atomic commits was part of the reason and also
mpv's geometry handling then was a lot rougher. We *shouldn't* need this
workaround anymore. The width/height values are only used exactly when
we need them. If mpv sets geometry on its own, it should still be the
right dimensions.

Related to the above, mpv never actually propertly handled the case
where width or height was equal to 0. According to the xdg-shell spec,
"If the width or height arguments are zero, it means the client should
decided its own window dimension." An example of a compositor doing this
is weston. It's, unsurprisingly, broken. Getting out of fullscreen or a
maximized state does not restore the old window size like it should. The
right way to handle this is to just return near the end of the function
if we have a 0 for either argument and before any geometry is set
(wl->geometry's width or height can never be zero). Luckily, state
changes are already being detected so they just trigger the goto when
needed.

Finally, db53f2d mistakenly removed the VO_EVENT_EXPOSE. There are edge
cases where this is needed and it's safer to just force a redraw here
when the window gets activated again. Just force wl->hidden to false
first and then trigger the expose.

video/out/wayland_common.c
@@ -796,7 +796,6 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
             is_fullscreen = true;
             break;
         case XDG_TOPLEVEL_STATE_RESIZING:
-            wl->pending_vo_events |= VO_EVENT_LIVE_RESIZING;
             break;
         case XDG_TOPLEVEL_STATE_ACTIVATED:
             is_activated = true;
@@ -839,15 +838,12 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
             wl->focused = !wl->focused;
             wl->pending_vo_events |= VO_EVENT_FOCUS;
         }
+        /* Just force a redraw to be on the safe side. */
+        if (wl->activated) {
+            wl->hidden = false;
+            wl->pending_vo_events |= VO_EVENT_EXPOSE;
+        }
     }
-
-    if (!(wl->pending_vo_events & VO_EVENT_LIVE_RESIZING))
-        vo_query_and_reset_events(wl->vo, VO_EVENT_LIVE_RESIZING);
-
-    int old_toplevel_width = wl->toplevel_width;
-    int old_toplevel_height = wl->toplevel_height;
-    wl->toplevel_width = width;
-    wl->toplevel_height = height;
 
     if (wl->scale_change) {
         wl_surface_set_buffer_scale(wl->surface, wl->scaling);
@@ -862,12 +858,12 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
         }
     }
 
-    if (old_toplevel_width == wl->toplevel_width && old_toplevel_height == wl->toplevel_height)
+    if (width == 0 || height == 0)
         return;
 
     if (!is_fullscreen && !is_maximized) {
         if (vo_opts->keepaspect && vo_opts->keepaspect_window) {
-            if (abs(wl->toplevel_width - old_toplevel_width) > abs(wl->toplevel_height - old_toplevel_height)) {
+            if (width > height) {
                 double scale_factor = (double)width / wl->reduced_width;
                 width = wl->reduced_width * scale_factor;
             } else {

video/out/wayland_common.h
@@ -50,8 +50,6 @@ struct vo_wayland_state {
     int gcd;
     int reduced_height;
     int reduced_width;
-    int toplevel_width;
-    int toplevel_height;
 
     /* State */
     bool activated;