dancingmirrors/dmpv

commit 61b58bedfd5fd2fc9b5b2fac538e41cf6f5c3918

egl_helpers: add support for debug contexts
With the recent refactor and quick look against the GLX code path, it's
fairly obvious, and trivial, how to add support for debug contexts.

Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

video/out/opengl/egl_helpers.c
@@ -161,6 +161,7 @@ static bool create_context(struct ra_ctx *ctx, EGLDisplay display,
     MP_DBG(ctx, "Chosen EGLConfig:\n");
     dump_egl_config(ctx->log, MSGL_DEBUG, display, config);
 
+    int ctx_flags = ctx->opts.debug ? EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR : 0;
     EGLContext *egl_ctx = NULL;
 
     if (!es) {
@@ -172,6 +173,7 @@ static bool create_context(struct ra_ctx *ctx, EGLDisplay display,
                 EGL_CONTEXT_MINOR_VERSION, MPGL_VER_GET_MINOR(ver),
                 EGL_CONTEXT_OPENGL_PROFILE_MASK,
                     ver >= 320 ? EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT : 0,
+                EGL_CONTEXT_FLAGS_KHR, ctx_flags,
                 EGL_NONE
             };
 
@@ -182,8 +184,10 @@ static bool create_context(struct ra_ctx *ctx, EGLDisplay display,
     }
     if (!egl_ctx) {
         // Fallback for EGL 1.4 without EGL_KHR_create_context or GLES
+        // Add the context flags only for GLES - GL has been attempted above
         EGLint attrs[] = {
             EGL_CONTEXT_CLIENT_VERSION, 2,
+            es ? EGL_CONTEXT_FLAGS_KHR : EGL_NONE, ctx_flags,
             EGL_NONE
         };
 

commit 51c38506bed40ecc168003c459544b5863c4daca

egl_helpers: ensure debug context attrs/bit exist
Regression from 61b58bedfd5fd2fc9b5b2fac538e41cf6f5c3918. Apparently,
Broadcom's EGL does not support the EGL_CONTEXT_FLAGS_KHR attribute nor
EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR. Just define these as EGL_NONE and 0
respectively if we do not have them.

video/out/opengl/egl_helpers.c
@@ -45,6 +45,15 @@
 typedef intptr_t EGLAttrib;
 #endif
 
+// Not every EGL provider (like RPI) has these.
+#ifndef EGL_CONTEXT_FLAGS_KHR
+#define EGL_CONTEXT_FLAGS_KHR EGL_NONE
+#endif
+
+#ifndef EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR
+#define EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR 0
+#endif
+
 struct mp_egl_config_attr {
     int attrib;
     const char *name;