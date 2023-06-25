rcombs/mpv

command: add sub-forced-only-cur prop

player/command.c
@@ -2901,6 +2901,18 @@ static int mp_property_sub_end(void *ctx, struct m_property *prop,
     return m_property_double_ro(action, arg, end);
 }
 
+static int mp_property_sub_forced_only_cur(void *ctx, struct m_property *prop,
+                                           int action, void *arg)
+{
+    MPContext *mpctx = ctx;
+    int ret = mpctx->opts->subs_rend->forced_subs_only;
+    if (ret == -1) {
+        struct track *track = mpctx->current_track[0][STREAM_SUB];
+        ret = track && track->forced_only_def;
+    }
+    return m_property_flag_ro(action, arg, ret);
+}
+
 static int mp_property_playlist_current_pos(void *ctx, struct m_property *prop,
                                             int action, void *arg)
 {
@@ -3677,6 +3689,7 @@ static const struct m_property mp_properties_base[] = {
         .priv = (void *)&(const int){SD_TEXT_TYPE_ASS}},
     {"sub-start", mp_property_sub_start},
     {"sub-end", mp_property_sub_end},
+    {"sub-forced-only-cur", mp_property_sub_forced_only_cur},
 
     {"vf", mp_property_vf},
     {"af", mp_property_af},

dancingmirrors/dmpv

player/command.c
@@ -2943,6 +2943,18 @@ static int mp_property_sub_end(void *ctx, struct m_property *prop,
     return m_property_double_ro(action, arg, end);
 }
 
+static int mp_property_sub_forced_only_cur(void *ctx, struct m_property *prop,
+                                           int action, void *arg)
+{
+    MPContext *mpctx = ctx;
+    int ret = mpctx->opts->subs_rend->forced_subs_only;
+    if (ret == -1) {
+        struct track *track = mpctx->current_track[0][STREAM_SUB];
+        ret = track && track->forced_only_def;
+    }
+    return m_property_bool_ro(action, arg, ret);
+}
+
 static int mp_property_playlist_current_pos(void *ctx, struct m_property *prop,
                                             int action, void *arg)
 {
@@ -3899,6 +3911,7 @@ static const struct m_property mp_properties_base[] = {
         .priv = (void *)&(const int){0}},
     {"secondary-sub-end", mp_property_sub_end,
         .priv = (void *)&(const int){1}},
+    {"sub-forced-only-cur", mp_property_sub_forced_only_cur},
 
     {"vf", mp_property_vf},
     {"af", mp_property_af},