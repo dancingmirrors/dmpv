rcombs/mpv

command: report auto-forced-only flag in track desc

player/command.c
@@ -1952,6 +1952,7 @@ static int get_track_entry(int item, int action, void *arg, void *ctx)
         {"albumart",    SUB_PROP_FLAG(track->attached_picture)},
         {"default",     SUB_PROP_FLAG(track->default_track)},
         {"forced",      SUB_PROP_FLAG(track->forced_track)},
+        {"auto-forced-only", SUB_PROP_FLAG(track->forced_only_def)},
         {"dependent",   SUB_PROP_FLAG(track->dependent_track)},
         {"visual-impaired",  SUB_PROP_FLAG(track->visual_impaired_track)},
         {"hearing-impaired", SUB_PROP_FLAG(track->hearing_impaired_track)},

dancingmirrors/dmpv

player/command.c
@@ -1971,6 +1971,7 @@ static int get_track_entry(int item, int action, void *arg, void *ctx)
         {"albumart",    SUB_PROP_BOOL(track->attached_picture)},
         {"default",     SUB_PROP_BOOL(track->default_track)},
         {"forced",      SUB_PROP_BOOL(track->forced_track)},
+        {"auto-forced-only", SUB_PROP_BOOL(track->forced_only_def)},
         {"dependent",   SUB_PROP_BOOL(track->dependent_track)},
         {"visual-impaired",  SUB_PROP_BOOL(track->visual_impaired_track)},
         {"hearing-impaired", SUB_PROP_BOOL(track->hearing_impaired_track)},