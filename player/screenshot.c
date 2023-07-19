dancingmirrors/dmpv

commit 618ce7a18343fd6507474f541a92a56482c3b9ff

player/screenshot: avoid non-sRGB spaces with --screenshot-tag-csp=no
If --screenshot-tag-csp=no, then there won't be any color tags in the
output space, so PNG and JXL screenshots should be written as sRGB
rather than the native space of the input video.

player/screenshot.c
@@ -347,7 +347,7 @@ static struct mp_image *screenshot_get(struct MPContext *mpctx, int mode,
         .subs = mode != 0,
         .osd = mode == MODE_FULL_WINDOW,
         .high_bit_depth = high_depth && imgopts->high_bit_depth,
-        .native_csp = image_writer_flexible_csp(imgopts),
+        .native_csp = imgopts->tag_csp && image_writer_flexible_csp(imgopts),
     };
     if (!mpctx->opts->screenshot_sw)
         vo_control(mpctx->video_out, VOCTRL_SCREENSHOT, &ctrl);

commit 66ab576892f255f681dfc0e1affb3aa72794559f

image_writer: move tag_csp check into image_writer_flexible_csp()
This ensures the sRGB fallback to happen in all situations involving
the image writer code, notably --screenshot-sw.

fixes: 618ce7a18343fd6507474f541a92a56482c3b9ff

player/screenshot.c
@@ -350,7 +350,7 @@ static struct mp_image *screenshot_get(struct MPContext *mpctx, int mode,
         .subs = mode != 0,
         .osd = mode == MODE_FULL_WINDOW,
         .high_bit_depth = high_depth && imgopts->high_bit_depth,
-        .native_csp = imgopts->tag_csp && image_writer_flexible_csp(imgopts),
+        .native_csp = image_writer_flexible_csp(imgopts),
     };
     if (!mpctx->opts->screenshot_sw)
         vo_control(mpctx->video_out, VOCTRL_SCREENSHOT, &ctrl);

video/image_writer.c
@@ -587,6 +587,8 @@ bool image_writer_high_depth(const struct image_writer_opts *opts)
 
 bool image_writer_flexible_csp(const struct image_writer_opts *opts)
 {
+    if (!opts->tag_csp)
+        return false;
     return false
 #if HAVE_JPEGXL
         || opts->format == AV_CODEC_ID_JPEGXL