f12f19503cc704a0f102d5cb72bd29131df852ab
llyyr <llyyr@yukari.in>
2024-03-04 17:30:28 +0530

    path: don't load any files if --no-config is passed
    `--no-config` should prevent loading any user files, whether it be
    config, cache, watch_later state etc. This functionality was changed by
    df758880e26c because internally `--no-config` is equivalent to passing
    `--config-dir=""` which resulted in cache and state being auto-detected
    even if `--no-config` was passed.
    
    Fixes: df758880e26c ("path: don't override "cache" and "state" paths with configdir")

--- options/path.c
+++ options/path.c
@@ -60,10 +60,14 @@ static const char *mp_get_platform_path(void *talloc_ctx,
     mp_assert(talloc_ctx);
 
     if (global->configdir && global->configdir[0]) {
+        // Return NULL for all platform paths if --no-config is passed
+        if (!global->configdir[0])
+            return NULL;
+
         // force all others to NULL, only first returns the path
         for (int n = 0; n < MP_ARRAY_SIZE(config_dirs); n++) {
             if (strcmp(config_dirs[n], type) == 0)
-                return n == 0 ? global->configdir : NULL;
+                return (n == 0) ? global->configdir : NULL;
         }
     }
 
