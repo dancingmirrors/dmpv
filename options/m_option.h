dac07ebb9041d7e020430f354be3c1e17a68cb10
sfan5 <sfan5@live.de>
2024-09-29 15:19:52 +0200

    options: enable handling --no-hwdec as --hwdec=no
    Reusing M_OPT_ALLOW_NO has the side-effect of applying to --ab-loop-[ab],
    which makes sense.

--- options/m_option.h
+++ options/m_option.h
@@ -458,6 +458,8 @@ char *format_file_size(int64_t size);
 #define M_OPT_DEFAULT_NAN       (1 << 23)
 
 // type time: string "no" maps to MP_NOPTS_VALUE (if unset, NOPTS is rejected)
+// and
+// parsing: "--no-opt" is parsed as "--opt=no"
 #define M_OPT_ALLOW_NO          (1 << 24)
 
 // type channels: disallow "auto" (still accept ""), limit list to at most 1 item.
