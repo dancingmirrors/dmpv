24b1704c367431a1a098b49b92c8ef44409e0e9e
Dudemanguy <random342@airmail.cc>
2025-03-06 10:11:36 -0600

    vo: replace driver->untimed with VO_CAP_UNTIMED

--- player/video.c
+++ player/video.c
@@ -420,7 +420,7 @@ static bool use_video_lookahead(struct MPContext *mpctx)
 {
     return mpctx->video_out &&
            !(mpctx->video_out->driver->caps & VO_CAP_NORETAIN) &&
-           !(mpctx->opts->untimed || mpctx->video_out->driver->untimed) &&
+           !(mpctx->opts->untimed || (mpctx->video_out->driver->caps & VO_CAP_UNTIMED)) &&
            !mpctx->opts->video_latency_hacks;
 }
 
@@ -637,7 +637,8 @@ static void update_avsync_before_frame(struct MPContext *mpctx)
          * If untimed is set always output frames immediately
          * without sleeping.
          */
-        if (mpctx->time_frame < -0.2 || opts->untimed || vo->driver->untimed)
+        if (mpctx->time_frame < -0.2 || opts->untimed ||
+            (vo->driver->caps & VO_CAP_UNTIMED))
             mpctx->time_frame = 0;
     }
 }
@@ -1233,7 +1234,7 @@ void write_video(struct MPContext *mpctx)
     struct vo_frame *frame = vo_frame_ref(&dummy);
 
     double diff = mpctx->past_frames[0].approx_duration;
-    if (opts->untimed || vo->driver->untimed)
+    if (opts->untimed || (vo->driver->caps & VO_CAP_UNTIMED))
         diff = -1; // disable frame dropping and aspects of frame timing
     if (diff >= 0) {
         diff /= mpctx->video_speed;
