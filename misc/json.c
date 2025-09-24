mpv-player/mpv

player/command: don't return ill-formed node if there is no default val
option-info/<name>/default-value would be initialized with zeroed
object, which is not valid for string typed options, as the would be set
to NULL.

Found by OSS-Fuzz.

misc/json.c
@@ -255,6 +255,8 @@ static const char special_escape[] = {
 
 static void write_json_str(bstr *b, unsigned char *str)
 {
+    assert(str);
+
     APPEND(b, "\"");
     while (1) {
         unsigned char *cur = str;

dancingmirrors/dmpv

misc/json.c
@@ -254,6 +254,8 @@ static const char special_escape[] = {
 
 static void write_json_str(bstr *b, unsigned char *str)
 {
+    mp_assert(str);
+
     APPEND(b, "\"");
     while (1) {
         unsigned char *cur = str;