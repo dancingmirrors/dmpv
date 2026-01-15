mpv-player/mpv

player/main: select msg output stream early
This avoids printing any stray messages in encode output stream.

--o is already pre-parse cli option which is designed to be parsed
before anything else is printed to output. So we can use that to force
stderr output if needed for encode mode.

common/encode_lavc.c
@@ -117,12 +117,6 @@ struct encode_lavc_context *encode_lavc_init(struct mpv_global *global)
     if (!strcmp(filename, "-"))
         filename = "pipe:1";
 
-    if (filename && (
-            !strcmp(filename, "/dev/stdout") ||
-            !strcmp(filename, "pipe:") ||
-            !strcmp(filename, "pipe:1")))
-        mp_msg_force_stderr(global, true);
-
     encode_lavc_discontinuity(ctx);
 
     p->muxer = avformat_alloc_context();
@@ -944,6 +938,19 @@ fail:
     MP_ERR(p, "error encoding at %s\n",
            frame ? av_ts2timestr(frame->pts, &p->encoder->time_base) : "EOF");
     return false;
+}
+
+void encoder_update_log(struct mpv_global *global)
+{
+    struct encode_opts *options = mp_get_config_group(NULL, global, &encode_config);
+    if (options->file && (!strcmp(options->file, "-") ||
+                          !strcmp(options->file, "/dev/stdout") ||
+                          !strcmp(options->file, "pipe:") ||
+                          !strcmp(options->file, "pipe:1")))
+    {
+        mp_msg_force_stderr(global, true);
+    }
+    talloc_free(options);
 }
 
 // vim: ts=4 sw=4 et

common/encode_lavc.h
@@ -111,4 +111,6 @@ bool encoder_encode(struct encoder_context *p, AVFrame *frame);
 // Caller needs to acquire encode_lavc_context.lock (or call it from on_ready).
 AVRational encoder_get_mux_timebase_unlocked(struct encoder_context *p);
 
+void encoder_update_log(struct mpv_global *global);
+
 #endif

dancingmirrors/dmpv

common/encode_lavc.c
@@ -121,12 +121,6 @@ struct encode_lavc_context *encode_lavc_init(struct dmpv_global *global)
     if (!strcmp(filename, "-"))
         filename = "pipe:1";
 
-    if (filename && (
-            !strcmp(filename, "/dev/stdout") ||
-            !strcmp(filename, "pipe:") ||
-            !strcmp(filename, "pipe:1")))
-        mp_msg_force_stderr(global, true);
-
     encode_lavc_discontinuity(ctx);
 
     p->muxer = avformat_alloc_context();
@@ -947,6 +941,19 @@ double encoder_get_offset(struct encoder_context *p)
     case AVMEDIA_TYPE_AUDIO: return p->options->aoffset;
     default:                 return 0;
     }
+}
+
+void encoder_update_log(struct dmpv_global *global)
+{
+    struct encode_opts *options = mp_get_config_group(NULL, global, &encode_config);
+    if (options->file && (!strcmp(options->file, "-") ||
+                          !strcmp(options->file, "/dev/stdout") ||
+                          !strcmp(options->file, "pipe:") ||
+                          !strcmp(options->file, "pipe:1")))
+    {
+        mp_msg_force_stderr(global, true);
+    }
+    talloc_free(options);
 }
 
 // vim: ts=4 sw=4 et

common/encode_lavc.h
@@ -107,4 +107,6 @@ AVRational encoder_get_mux_timebase_unlocked(struct encoder_context *p);
 
 double encoder_get_offset(struct encoder_context *p);
 
+void encoder_update_log(struct dmpv_global *global);
+
 #endif