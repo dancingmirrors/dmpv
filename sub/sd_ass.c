debb728b326d9e2b57fe587dde9f50e73fa6809e
dancingmirrors <dancingmirrors@icloud.com>
2026-01-31 08:58:39 -0700

    Drop a libass ifdef that's already below our requirements.

--- sub/sd_ass.c
+++ sub/sd_ass.c
@@ -432,10 +432,8 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         set_force_flags |= ASS_OVERRIDE_BIT_SELECTIVE_FONT_SCALE;
     if (converted)
         set_force_flags |= ASS_OVERRIDE_BIT_ALIGNMENT;
-#if LIBASS_VERSION >= 0x01306000
     if ((converted || opts->ass_style_override) && opts->ass_justify)
         set_force_flags |= ASS_OVERRIDE_BIT_JUSTIFY;
-#endif
     ass_set_selective_style_override_enabled(priv, set_force_flags);
     ASS_Style style = {0};
     mp_ass_set_style(&style, 288, opts->sub_style);
