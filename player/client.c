mpv-player/mpv

player/client: fix typo in description of MPV_ERROR_UNINITIALIZED

player/client.c
@@ -2063,7 +2063,7 @@ static const char *const err_table[] = {
     [-MPV_ERROR_SUCCESS] = "success",
     [-MPV_ERROR_EVENT_QUEUE_FULL] = "event queue full",
     [-MPV_ERROR_NOMEM] = "memory allocation failed",
-    [-MPV_ERROR_UNINITIALIZED] = "core not uninitialized",
+    [-MPV_ERROR_UNINITIALIZED] = "core not initialized",
     [-MPV_ERROR_INVALID_PARAMETER] = "invalid parameter",
     [-MPV_ERROR_OPTION_NOT_FOUND] = "option not found",
     [-MPV_ERROR_OPTION_FORMAT] = "unsupported format for accessing option",

dancingmirrors/dmpv

player/client.c
@@ -2071,7 +2071,7 @@ static const char *const err_table[] = {
     [-DMPV_ERROR_SUCCESS] = "success",
     [-DMPV_ERROR_EVENT_QUEUE_FULL] = "event queue full",
     [-DMPV_ERROR_NOMEM] = "memory allocation failed",
-    [-DMPV_ERROR_UNINITIALIZED] = "core not uninitialized",
+    [-DMPV_ERROR_UNINITIALIZED] = "core not initialized",
     [-DMPV_ERROR_INVALID_PARAMETER] = "invalid parameter",
     [-DMPV_ERROR_OPTION_NOT_FOUND] = "option not found",
     [-DMPV_ERROR_OPTION_FORMAT] = "unsupported format for accessing option",