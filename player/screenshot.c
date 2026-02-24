0ec3b39df4d47e4736a92416d7e32e8563e1aaa7
NRK <nrk@disroot.org>
2026-02-24 13:55:26 +0000

    screenshot: percent decode filename if it's a URI

--- player/screenshot.c
+++ player/screenshot.c
@@ -42,6 +42,7 @@
 #include "video/image_writer.h"
 #include "video/sws_utils.h"
 #include "sub/osd.h"
+#include "stream/stream.h"
 
 #include "video/csputils.h"
 
@@ -174,8 +175,12 @@ static char *create_fname(struct MPContext *mpctx, char *template,
         case 'f':
         case 'F': {
             char *video_file = NULL;
-            if (mpctx->filename)
-                video_file = mp_basename(mpctx->filename);
+            if (mpctx->filename) {
+                if (mp_is_url(bstr0(mpctx->filename)))
+                    video_file = mp_basename(mp_url_unescape(res, mpctx->filename));
+                else
+                    video_file = mp_basename(mpctx->filename);
+            }
 
             if (!video_file)
                 video_file = "NO_FILE";
